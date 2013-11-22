2aa157fcb92e60b9937fa5efdf10eb589718eae6
Paul Ionkin <paul.ionkin@gmail.com>
2013-11-22 11:37:00 +0100

    use relative path for headers

--- include/crypto/cryptopp.h
+++ include/crypto/cryptopp.h
@@ -20,16 +20,17 @@ DEALINGS IN THE SOFTWARE.
 #ifndef CRYPTOCRYPTOPP_H
 #define CRYPTOCRYPTOPP_H 1
 
-#include <cryptlib.h>
-#include <modes.h>
-#include <integer.h>
-#include <aes.h>
-#include <osrng.h>
-#include <sha.h>
-#include <rsa.h>
-#include <crc.h>
-#include <nbtheory.h>
-#include <algparam.h>
+#include "config.h"
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/integer.h>
+#include <cryptopp/aes.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/rsa.h>
+#include <cryptopp/crc.h>
+#include <cryptopp/nbtheory.h>
+#include <cryptopp/algparam.h>
 
 using namespace CryptoPP;
 
