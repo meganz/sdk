a6aa2cfa1bc554e5cdde1df4df9993f4bf3cf826
javiergm1983 <jg@mega.co.nz>
2014-12-11 13:51:28 +0100

    License in WP8 example and binding

--- examples/wp8/MegaApp/MegaApp/Converters/FileTypeToImageConverter.cs
+++ examples/wp8/MegaApp/MegaApp/Converters/FileTypeToImageConverter.cs
@@ -1,4 +1,25 @@
-﻿using System.IO;
+﻿/**
+ * @file FileTypeToImageConverter.cs
+ * @brief Class for convert the file type to a image icon.
+ *
+ * (c) 2013-2014 by Mega Limited, Auckland, New Zealand
+ *
+ * This file is part of the MEGA SDK - Client Access Engine.
+ *
+ * Applications using the MEGA API must present a valid application key
+ * and comply with the the rules set forth in the Terms of Service.
+ *
+ * The MEGA SDK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright Simplified (2-clause) BSD License.
+ *
+ * You should have received a copy of the license along with this
+ * program.
+ */
+
+using System.IO;
 using System;
 using System.Globalization;
 using System.Windows.Data;
