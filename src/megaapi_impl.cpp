88db14e1667178c13344c6a6912b7c0c458c613a
Javier Serrano <js@mega.co.nz>
2015-03-13 17:38:18 +0100

    Compilation fix (Win)

--- src/megaapi_impl.cpp
+++ src/megaapi_impl.cpp
@@ -990,7 +990,7 @@ MegaContactRequestPrivate::MegaContactRequestPrivate(PendingContactRequest *requ
     }
     else
     {
-        status = MegaContactRequest::STATUS_PENDING;
+        status = MegaContactRequest::STATUS_UNRESOLVED;
     }
 
     outgoing = request->isoutgoing;
