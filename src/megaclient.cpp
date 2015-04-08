a01e86d7b93687052db244b93079ecaf8d147ff0
Victor Teniente <vtmateos@gmail.com>
2015-04-08 13:45:23 +0200

    Fix logging issues

--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -2495,7 +2495,7 @@ void MegaClient::updatesc()
                 char base64[12];
                 if ((*it)->removed() && (*it)->dbid)
                 {
-                    LOG_verbose << "Removing pcr from database: " << (Base64::btoa((byte*)&((*it)->id),MegaClient::NODEHANDLE,base64) ? base64 : "");
+                    LOG_verbose << "Removing pcr from database: " << (Base64::btoa((byte*)&((*it)->id),MegaClient::PCRHANDLE,base64) ? base64 : "");
                     if (!(complete = sctable->del((*it)->dbid)))
                     {
                         break;
@@ -2503,7 +2503,7 @@ void MegaClient::updatesc()
                 }
                 else if (!(*it)->removed())
                 {
-                    LOG_verbose << "Adding pcr to database: " << (Base64::btoa((byte*)&((*it)->id),MegaClient::NODEHANDLE,base64) ? base64 : "");
+                    LOG_verbose << "Adding pcr to database: " << (Base64::btoa((byte*)&((*it)->id),MegaClient::PCRHANDLE,base64) ? base64 : "");
                     if (!(complete = sctable->put(CACHEDPCR, *it, &key)))
                     {
                         break;
