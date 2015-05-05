e6b9355f6ecadff80a00e6f410b4941c3f3dffb2
javiserrano <fjaviersr@gmail.com>
2015-05-06 01:49:09 +0200

    Formatting

--- bindings/java/nz/mega/sdk/DelegateMegaGlobalListener.java
+++ bindings/java/nz/mega/sdk/DelegateMegaGlobalListener.java
@@ -3,53 +3,53 @@ package nz.mega.sdk;
 import java.util.ArrayList;
 
 class DelegateMegaGlobalListener extends MegaGlobalListener {
-	MegaApiJava megaApi;
-	MegaGlobalListenerInterface listener;
+    MegaApiJava megaApi;
+    MegaGlobalListenerInterface listener;
 
-	DelegateMegaGlobalListener(MegaApiJava megaApi,
-			MegaGlobalListenerInterface listener) {
-		this.megaApi = megaApi;
-		this.listener = listener;
-	}
+    DelegateMegaGlobalListener(MegaApiJava megaApi,
+            MegaGlobalListenerInterface listener) {
+        this.megaApi = megaApi;
+        this.listener = listener;
+    }
 
-	MegaGlobalListenerInterface getUserListener() {
-		return listener;
-	}
+    MegaGlobalListenerInterface getUserListener() {
+        return listener;
+    }
 
-	@Override
-	public void onUsersUpdate(MegaApi api, MegaUserList userList) {
-		if (listener != null) {
-			final ArrayList<MegaUser> users = MegaApiJava
-					.userListToArray(userList);
-			megaApi.runCallback(new Runnable() {
-				public void run() {
-					listener.onUsersUpdate(megaApi, users);
-				}
-			});
-		}
-	}
+    @Override
+    public void onUsersUpdate(MegaApi api, MegaUserList userList) {
+        if (listener != null) {
+            final ArrayList<MegaUser> users = MegaApiJava
+                    .userListToArray(userList);
+            megaApi.runCallback(new Runnable() {
+                public void run() {
+                    listener.onUsersUpdate(megaApi, users);
+                }
+            });
+        }
+    }
 
-	@Override
-	public void onNodesUpdate(MegaApi api, MegaNodeList nodeList) {
-		if (listener != null) {
-			final ArrayList<MegaNode> nodes = MegaApiJava
-					.nodeListToArray(nodeList);
-			megaApi.runCallback(new Runnable() {
-				public void run() {
-					listener.onNodesUpdate(megaApi, nodes);
-				}
-			});
-		}
-	}
+    @Override
+    public void onNodesUpdate(MegaApi api, MegaNodeList nodeList) {
+        if (listener != null) {
+            final ArrayList<MegaNode> nodes = MegaApiJava
+                    .nodeListToArray(nodeList);
+            megaApi.runCallback(new Runnable() {
+                public void run() {
+                    listener.onNodesUpdate(megaApi, nodes);
+                }
+            });
+        }
+    }
 
-	@Override
-	public void onReloadNeeded(MegaApi api) {
-		if (listener != null) {
-			megaApi.runCallback(new Runnable() {
-				public void run() {
-					listener.onReloadNeeded(megaApi);
-				}
-			});
-		}
-	}
+    @Override
+    public void onReloadNeeded(MegaApi api) {
+        if (listener != null) {
+            megaApi.runCallback(new Runnable() {
+                public void run() {
+                    listener.onReloadNeeded(megaApi);
+                }
+            });
+        }
+    }
 }
