d688521ec1663a2d0d1d4c9e4bf75e64ba5ce823
Paul Ionkin <paul.ionkin@gmail.com>
2015-05-06 12:08:56 +0200

    New package name

--- contrib/build_sdk.sh
+++ contrib/build_sdk.sh
@@ -67,7 +67,8 @@ print_distro_help()
     type apt-get >/dev/null 2>&1
     local exit_code=$?
     if [ $exit_code -eq 0 ]; then
-        echo "Please execute the following command:  sudo apt-get install gcc c++ libtool unzip autoconf make wget"
+        echo "Please execute the following command:  sudo apt-get install gcc c++ libtool-bin unzip autoconf make wget"
+        echo " (or 'libtool' on older Debian / Ubuntu distro versions)"
         return
     fi
 }
