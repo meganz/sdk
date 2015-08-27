0627865dea567c3a986324b89683ae71a84c78c3
Patrick Baird <patrickjbaird@gmail.com>
2015-08-27 13:28:59 +1200

    Added JavaDoc comments

--- bindings/java/nz/mega/sdk/DelegateMegaGlobalListener.java
+++ bindings/java/nz/mega/sdk/DelegateMegaGlobalListener.java
@@ -16,6 +16,19 @@ class DelegateMegaGlobalListener extends MegaGlobalListener {
         return listener;
     }
 
+
+    /**
+     * This function is called when there are new or updated contacts in the account. 
+     * The SDK retains the ownership of the MegaUserList in the second parameter. 
+     * The list and all the MegaUser objects that it contains will be valid until this function returns. 
+     * If you want to save the list, use MegaUserList::copy. 
+     * If you want to save only some of the MegaUser objects, use MegaUser::copy for those objects.
+     *  
+     * @param userList
+     *            List of new or updated Contacts
+     * @param api
+     *            Mega Java API connected to account
+     */
     @Override
     public void onUsersUpdate(MegaApi api, MegaUserList userList) {
         if (listener != null) {
@@ -29,6 +42,20 @@ class DelegateMegaGlobalListener extends MegaGlobalListener {
         }
     }
 
+
+    /**
+     * This function is called when there are new or updated nodes in the account. 
+     * When the full account is reloaded or a large number of server notifications arrives at once, the second parameter will be NULL.
+     * The SDK retains the ownership of the MegaNodeList in the second parameter. 
+     * The list and all the MegaNode objects that it contains will be valid until this function returns. 
+     * If you want to save the list, use MegaNodeList::copy. 
+     * If you want to save only some of the MegaNode objects, use MegaNode::copy for those nodes.
+     *  
+     * @param nodeList
+     *            List of new or updated Nodes
+     * @param api
+     *            API connected to account
+     */
     @Override
     public void onNodesUpdate(MegaApi api, MegaNodeList nodeList) {
         if (listener != null) {
@@ -42,6 +69,13 @@ class DelegateMegaGlobalListener extends MegaGlobalListener {
         }
     }
 
+    /**
+     * This function is called when an inconsistency is detected in the local cache. 
+     * You should call MegaApi::fetchNodes when this callback is received
+     *  
+     * @param api
+     *            API connected to account
+     */
     @Override
     public void onReloadNeeded(MegaApi api) {
         if (listener != null) {
