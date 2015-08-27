63e56fd3c02834047257311c9d46dc1f5bd47cfc
Patrick Baird <patrickjbaird@gmail.com>
2015-08-27 13:42:56 +1200

    Added JavaDoc comments

--- bindings/java/nz/mega/sdk/DelegateMegaListener.java
+++ bindings/java/nz/mega/sdk/DelegateMegaListener.java
@@ -15,6 +15,17 @@ class DelegateMegaListener extends MegaListener {
         return listener;
     }
 
+    /**
+     * This function is called when a request is about to start being processed.
+     * The SDK retains the ownership of the request parameter. 
+     * Don't use it after this functions returns.
+     * The api object is the one created by the application, it will be valid until the application deletes it.
+     *  
+     * @param request
+     *            Information about the request
+     * @param api
+     *            API that started the request
+     */
     @Override
     public void onRequestStart(MegaApi api, MegaRequest request) {
         if (listener != null) {
@@ -27,6 +38,21 @@ class DelegateMegaListener extends MegaListener {
         }
     }
 
+    /**
+     * This function is called when a request has finished.
+     * There won't be more callbacks about this request.
+     * The last parameter provides the result of the request.
+     * If the request finished without problems, the error code will be API_OKThe SDK retains the ownership of the request and error parameters. 
+     * Don't use them after this functions returns.
+     * The api object is the one created by the application, it will be valid until the application deletes it.
+     *  
+     * @param request
+     *            Information about the request
+     * @param api
+     *            API that started the request
+     * @param e
+     *            Error Information
+     */
     @Override
     public void onRequestFinish(MegaApi api, MegaRequest request, MegaError e) {
         if (listener != null) {
@@ -42,6 +68,20 @@ class DelegateMegaListener extends MegaListener {
         }
     }
 
+    /**
+     * This function is called when there is a temporary error processing a request. 
+     * The request continues after this callback, so expect more MegaRequestListener::onRequestTemporaryError 
+     * or a MegaRequestListener::onRequestFinish callbackThe SDK retains the ownership of the request and error parameters. 
+     * Don't use them after this functions returns.
+     * The api object is the one created by the application, it will be valid until the application deletes it.
+     *  
+     * @param request
+     *            Information about the request
+     * @param api
+     *            API that started the request
+     * @param e
+     *            Error Information
+     */
     @Override
     public void onRequestTemporaryError(MegaApi api, MegaRequest request, MegaError e) {
         if (listener != null) {
@@ -55,6 +95,17 @@ class DelegateMegaListener extends MegaListener {
         }
     }
 
+    /**
+     * This function is called when a transfer is about to start being processed. 
+     * The SDK retains the ownership of the transfer parameter. 
+     * Don't use it after this functions returns.
+     * The api object is the one created by the application, it will be valid until the application deletes it.
+     *  
+     * @param transfer
+     *            Information about the transfer
+     * @param api
+     *            API that started the request
+     */
     @Override
     public void onTransferStart(MegaApi api, MegaTransfer transfer) {
         if (listener != null) {
@@ -67,6 +118,22 @@ class DelegateMegaListener extends MegaListener {
         }
     }
 
+    /**
+     * This function is called when a transfer has finished.
+     * The SDK retains the ownership of the transfer and error parameters.
+     * Don't use them after this functions returns.
+     * The api object is the one created by the application, it will be valid until the application deletes it.
+     * There won't be more callbacks about this transfer. 
+     * The last parameter provides the result of the transfer. 
+     * If the transfer finished without problems, the error code will be API_OK
+     *  
+     * @param transfer
+     *            Information about the transfer
+     * @param api
+     *            API that started the request
+     * @param e
+     *            Error Information
+     */
     @Override
     public void onTransferFinish(MegaApi api, MegaTransfer transfer, MegaError e) {
         if (listener != null) {
@@ -80,6 +147,16 @@ class DelegateMegaListener extends MegaListener {
         }
     }
 
+    /**
+     * This function is called to inform about the progress of a transfer. 
+     * The SDK retains the ownership of the transfer parameter. Don't use it after this functions returns.
+     * The api object is the one created by the application, it will be valid until the application deletes it.
+     *  
+     * @param transfer
+     *            Information about the transfer
+     * @param api
+     *            API that started the request
+     */
     @Override
     public void onTransferUpdate(MegaApi api, MegaTransfer transfer) {
         if (listener != null) {
@@ -91,7 +168,20 @@ class DelegateMegaListener extends MegaListener {
             });
         }
     }
-
+    
+    /**
+     * This function is called when there is a temporary error processing a transfer. 
+     * The transfer continues after this callback, so expect more MegaTransferListener::onTransferTemporaryError 
+     * or a MegaTransferListener::onTransferFinish callbackThe SDK retains the ownership of the transfer and error parameters.
+     * Don't use them after this functions returns.
+     *  
+     * @param transfer
+     *            Information about the transfer
+     * @param api
+     *            API that started the request
+     * @param e
+     *            Error Information
+     */
     @Override
     public void onTransferTemporaryError(MegaApi api, MegaTransfer transfer, MegaError e) {
         if (listener != null) {
@@ -105,6 +195,18 @@ class DelegateMegaListener extends MegaListener {
         }
     }
 
+   /**
+     * This function is called when there are new or updated contacts in the account. 
+     * The SDK retains the ownership of the MegaUserList in the second parameter.
+     * The list and all the MegaUser objects that it contains will be valid until this function returns. 
+     * If you want to save the list, use MegaUserList::copy.
+     * If you want to save only some of the MegaUser objects, use MegaUser::copy for those objects.
+     *  
+     * @param api
+     *            API that started the request
+     * @param userList
+     *            List that contains new or updated contacts
+     */
     @Override
     public void onUsersUpdate(MegaApi api, MegaUserList userList) {
         if (listener != null) {
@@ -117,6 +219,19 @@ class DelegateMegaListener extends MegaListener {
         }
     }
 
+   /**
+     * This function is called when there are new or updated nodes in the account. 
+     * When the full account is reloaded or a large number of server notifications arrives at once, the second parameter will be NULL.
+     * The SDK retains the ownership of the MegaNodeList in the second parameter. 
+     * The list and all the MegaNode objects that it contains will be valid until this function returns. 
+     * If you want to save the list, use MegaNodeList::copy. 
+     * If you want to save only some of the MegaNode objects, use MegaNode::copy for those nodes.
+     *  
+     * @param api
+     *            API that started the request
+     * @param nodeList
+     *            List that contains new or updated nodes
+     */
     @Override
     public void onNodesUpdate(MegaApi api, MegaNodeList nodeList) {
         if (listener != null) {
@@ -128,7 +243,14 @@ class DelegateMegaListener extends MegaListener {
             });
         }
     }
-
+    
+   /**
+     * This function is called when an inconsistency is detected in the local cache. 
+     * You should call MegaApi::fetchNodes when this callback is received
+     *  
+     * @param api
+     *            API that started the request
+     */
     @Override
     public void onReloadNeeded(MegaApi api) {
         if (listener != null) {
