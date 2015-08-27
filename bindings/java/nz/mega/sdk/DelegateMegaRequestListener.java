53e7e658077b470fb845125e16e7ea9a54ba78d9
Patrick Baird <patrickjbaird@gmail.com>
2015-08-27 13:51:51 +1200

    Added JavaDoc comments

--- bindings/java/nz/mega/sdk/DelegateMegaRequestListener.java
+++ bindings/java/nz/mega/sdk/DelegateMegaRequestListener.java
@@ -16,6 +16,17 @@ class DelegateMegaRequestListener extends MegaRequestListener {
         return listener;
     }
 
+    /**
+     * This function is called when a request is about to start being processed.
+     * The SDK retains the ownership of the request parameter. 
+     * Don't use it after this functions returns.
+     * The api object is the one created by the application, it will be valid until the application deletes it.
+     *  
+     * @param request
+     *            Information about the request
+     * @param api
+     *            API that started the request
+     */
     @Override
     public void onRequestStart(MegaApi api, MegaRequest request) {
         if (listener != null) {
@@ -28,6 +39,18 @@ class DelegateMegaRequestListener extends MegaRequestListener {
         }
     }
 
+    /**
+     * This function is called to inform about the progres of a request. 
+     * Currently, this callback is only used for fetchNodes (MegaRequest::TYPE_FETCH_NODES)
+     * requestsThe SDK retains the ownership of the request parameter. 
+     * Don't use it after this functions returns.
+     * The api object is the one created by the application, it will be valid until the application deletes it.
+     *  
+     * @param request
+     *            Information about the request
+     * @param api
+     *            API that started the request
+     */
     @Override
     public void onRequestUpdate(MegaApi api, MegaRequest request) {
         if (listener != null) {
@@ -40,6 +63,21 @@ class DelegateMegaRequestListener extends MegaRequestListener {
         }
     }
 
+    /**
+     * This function is called when a request has finished.
+     * There won't be more callbacks about this request.
+     * The last parameter provides the result of the request.
+     * If the request finished without problems, the error code will be API_OKThe SDK retains the ownership of the request and error parameters. 
+     * Don't use them after this functions returns.
+     * The api object is the one created by the application, it will be valid until the application deletes it.
+     *  
+     * @param request
+     *            Information about the request
+     * @param api
+     *            API that started the request
+     * @param e
+     *            Error Information
+     */
     @Override
     public void onRequestFinish(MegaApi api, MegaRequest request, MegaError e) {
         if (listener != null) {
@@ -56,6 +94,20 @@ class DelegateMegaRequestListener extends MegaRequestListener {
         }
     }
 
+    /**
+     * This function is called when there is a temporary error processing a request. 
+     * The request continues after this callback, so expect more MegaRequestListener::onRequestTemporaryError 
+     * or a MegaRequestListener::onRequestFinish callbackThe SDK retains the ownership of the request and error parameters. 
+     * Don't use them after this functions returns.
+     * The api object is the one created by the application, it will be valid until the application deletes it.
+     *  
+     * @param request
+     *            Information about the request
+     * @param api
+     *            API that started the request
+     * @param e
+     *            Error Information
+     */
     @Override
     public void onRequestTemporaryError(MegaApi api, MegaRequest request, MegaError e) {
         if (listener != null) {
