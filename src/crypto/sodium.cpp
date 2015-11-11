927c59d3f149c17349100355ded51932a33afd0a
Sergio Hernández <shs@mega.co.nz>
2015-11-11 11:21:47 +0100

    Initial commit
    - Adjust `sign()` to effectively returns the signature of the message
    - Fix methods documentation
    - Formatting style: brackets, returns...

--- src/crypto/sodium.cpp
+++ src/crypto/sodium.cpp
@@ -23,79 +23,84 @@
 #include "mega.h"
 
 #ifdef USE_SODIUM
-namespace mega {
+namespace mega
+{
 
 CryptoPP::AutoSeededRandomPool EdDSA::rng;
 
 // Initialise libsodium crypto system.
-void EdDSA::init() {
+void EdDSA::init()
+{
     sodium_init();
 }
 
 
 // Sets a private key seed from a buffer.
-void EdDSA::setKeySeed(const char* data) {
+void EdDSA::setKeySeed(const char* data)
+{
     memcpy(this->keySeed, data, crypto_sign_SEEDBYTES);
 }
 
 
 // Computes the signature of a message.
-int EdDSA::sign(unsigned char* msg, unsigned long long msglen, char* sig) {
+int EdDSA::sign(unsigned char* msg, unsigned long long msglen,
+                unsigned char* sig, unsigned long long siglen)
+{
+    if (!sig || siglen != msglen + crypto_sign_BYTES)
+    {
+        // Wrong allocated space for signature
+        return 0;
+    }
+
     unsigned char* pubKey = (unsigned char*)malloc(crypto_sign_PUBLICKEYBYTES);
-    if (pubKey == NULL) {
+    if (pubKey == NULL)
+    {
         // Something went wrong allocating the memory.
-        return(0);
+        return 0;
     }
     unsigned char* privKey = (unsigned char*)malloc(crypto_sign_SECRETKEYBYTES);
     if (privKey == NULL) {
         // Something went wrong allocating the memory.
         free(pubKey);
-        return(0);
+        return 0;
     }
     int check = 0;
     check = crypto_sign_seed_keypair(pubKey, privKey,
                                      (const unsigned char*)this->keySeed);
-    if (check != 0) {
+    if (check != 0)
+    {
         // Something went wrong deriving keys.
-        return(0);
-    }
-    unsigned long long bufferlen = 0;
-    unsigned char* sigbuffer = (unsigned char*)malloc(msglen + crypto_sign_BYTES);
-    if (sigbuffer == NULL) {
-        // Something went wrong allocating the memory.
         free(pubKey);
         free(privKey);
-        return(0);
+        return 0;
     }
-    check = crypto_sign(sigbuffer, &bufferlen, (const unsigned char*)msg, msglen,
+
+    check = crypto_sign(sig, &siglen, (const unsigned char*)msg, msglen,
                         (const unsigned char*)privKey);
-    if (check != 0) {
-        // Something went wrong signing the message.
-        free(sigbuffer);
-        free(pubKey);
-        free(privKey);
-        return(0);
-    }
-    free(sigbuffer);
+
     free(pubKey);
     free(privKey);
-    return(bufferlen);
+
+    return check ? 0 : siglen;
 }
 
 
 // Verifies the signature of a message.
 int EdDSA::verify(const unsigned char* msg, unsigned long long msglen,
-                  const unsigned char* sig, const unsigned char* pubKey) {
+                  const unsigned char* sig, const unsigned char* pubKey)
+{
     unsigned char* msgbuffer = (unsigned char*)malloc(msglen + crypto_sign_BYTES);
-    if (msgbuffer == NULL) {
+    if (msgbuffer == NULL)
+    {
         // Something went wrong allocating the memory.
-        return(0);
+        return 0;
     }
     unsigned char* signedmsg = (unsigned char*)malloc(msglen + crypto_sign_BYTES);
-    if (signedmsg == NULL) {
+    if (signedmsg == NULL)
+    {
         // Something went wrong allocating the memory.
         free(msgbuffer);
-        return(0);
+        return 0;
     }
 
     // Assemble signed message in one array.
@@ -107,52 +112,55 @@ int EdDSA::verify(const unsigned char* msg, unsigned long long msglen,
                                   signedmsg, msglen + crypto_sign_BYTES, pubKey);
     free(msgbuffer);
     free(signedmsg);
-    if (result) {
-        return(1);
-    } else {
-        // crypto_sign_open() returns -1 on failure.
-        return(0);
-    }
+
+    // crypto_sign_open() returns 0 on success.
+    return result ? 1 : 0;
 }
 
 
 // Generates a new Ed25519 private key seed. The key seed is stored in the object.
-int EdDSA::genKeySeed(unsigned char* privKey) {
+int EdDSA::genKeySeed(unsigned char* privKey)
+{
     // Make space for a new key seed (if not present).
-    if (!this->keySeed) {
+    if (!this->keySeed)
+    {
         this->keySeed = (unsigned char*)malloc(crypto_sign_SEEDBYTES);
-        if (this->keySeed == NULL) {
+        if (this->keySeed == NULL)
+        {
             // Something went wrong allocating the memory.
-            return(0);
+            return 0;
         }
     }
+
     // Now make the new key seed.
     this->rng.GenerateBlock(this->keySeed, crypto_sign_SEEDBYTES);
+
     // Copy it to privKey before returning.
     if (privKey)
     {
         memcpy(privKey, this->keySeed, crypto_sign_SEEDBYTES);
     }
-    return(1);
+
+    return 1;
 }
 
 
 // Derives the Ed25519 public key from the stored private key seed.
-int EdDSA::publicKey(unsigned char* pubKey) {
+int EdDSA::publicKey(unsigned char* pubKey)
+{
     unsigned char* privKey = (unsigned char*)malloc(crypto_sign_SECRETKEYBYTES);
-    if (privKey == NULL) {
+    if (privKey == NULL)
+    {
         // Something went wrong allocating the memory.
-        return(0);
+        return 0;
     }
+
     int check = crypto_sign_seed_keypair(pubKey, privKey,
                                          (const unsigned char*)this->keySeed);
-    if (check != 0) {
-        // Something went wrong deriving keys.
-        free(privKey);
-        return(0);
-    }
+
     free(privKey);
-    return(1);
+
+    return check ? 0 : 1;
 }
 
 } // namespace
