1db15da8321ce937fc467d1fe838085a8f1c2ba5
Javier Serrano <js@mega.co.nz>
2016-02-29 19:39:13 +0100

    Updated the build scripts for the SDK in the Android example app

--- examples/android/ExampleApp/jni/mega/Android.mk
+++ examples/android/ExampleApp/jni/mega/Android.mk
@@ -3,19 +3,19 @@ LOCAL_PATH := $(call my-dir)
 local_c_includes := \
         $(LOCAL_PATH) \
         $(LOCAL_PATH)/.. \
+        $(LOCAL_PATH)/sdk \
         $(LOCAL_PATH)/sdk/include \
         $(LOCAL_PATH)/sdk/include/mega/posix \
-        $(LOCAL_PATH)/sdk/third_party/utf8proc \
         $(LOCAL_PATH)/sdk/third_party/glob \
         $(LOCAL_PATH)/android
 
 include $(CLEAR_VARS)
 include $(LOCAL_PATH)/Makefile.inc
 LOCAL_MODULE    := megasdk
-LOCAL_CFLAGS := -fexceptions -frtti -fvisibility=hidden -fvisibility-inlines-hidden -fdata-sections -ffunction-sections -DUSE_PTHREAD
+LOCAL_CFLAGS := -fexceptions -frtti -fvisibility=hidden -fvisibility-inlines-hidden -fdata-sections -ffunction-sections -DDEBUG
 LOCAL_SRC_FILES := $(CPP_SOURCES) $(C_SOURCES) $(C_WRAPPER_SOURCES)
 LOCAL_C_INCLUDES += $(local_c_includes)
-LOCAL_STATIC_LIBRARIES := curl cryptopp sodium sqlite
+LOCAL_STATIC_LIBRARIES := curl cryptopp sqlite
 include $(BUILD_STATIC_LIBRARY)
 
 
@@ -25,7 +25,7 @@ include $(BUILD_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 include $(LOCAL_PATH)/Makefile.inc
 LOCAL_MODULE    := mega
-LOCAL_CFLAGS := -fexceptions -frtti -fdata-sections -ffunction-sections -DUSE_PTHREAD
+LOCAL_CFLAGS := -fexceptions -frtti -fdata-sections -ffunction-sections -DDEBUG
 LOCAL_SRC_FILES := $(JAVA_WRAPS)
 LOCAL_C_INCLUDES += -fexceptions -frtti $(local_c_includes)
 LOCAL_LDLIBS := -lm -lz -llog
