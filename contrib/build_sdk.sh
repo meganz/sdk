841b4c97af47c904a3e92c5bb3dc2998087fd4dc
Pablo Martin <pm@mega.nz>
2016-03-14 19:01:53 +0100

    corrected file name GNUMakefile when customizing cryptopp compilation

--- contrib/build_sdk.sh
+++ contrib/build_sdk.sh
@@ -313,7 +313,7 @@ cryptopp_pkg() {
         unzip -o $file -d $cryptopp_dir || exit 1
     fi
     #modify Makefile so that it does not use specific cpu architecture optimizations
-    sed "s#CXXFLAGS += -march=native#CXXFLAGS += #g" -i $cryptopp_dir/GnuMakefile
+    sed "s#CXXFLAGS += -march=native#CXXFLAGS += #g" -i $cryptopp_dir/GNUmakefile
     package_build $name $cryptopp_dir static
     package_install $name $cryptopp_dir $install_dir
 }
