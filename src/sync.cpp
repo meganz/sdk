ee00618addd6f4a052bab51598cb836cedd937bf
Javier Serrano <js@mega.co.nz>
2016-08-23 12:30:18 +0200

    Safe deletion of the dirnotify object on Windows

--- src/sync.cpp
+++ src/sync.cpp
@@ -56,7 +56,7 @@ Sync::Sync(MegaClient* cclient, string* crootpath, const char* cdebris,
         debris = cdebris;
         client->fsaccess->path2local(&debris, &localdebris);
 
-        dirnotify = client->fsaccess->newdirnotify(crootpath, &localdebris);
+        dirnotify = auto_ptr<DirNotify>(client->fsaccess->newdirnotify(crootpath, &localdebris));
 
         localdebris.insert(0, client->fsaccess->localseparator);
         localdebris.insert(0, *crootpath);
@@ -66,7 +66,7 @@ Sync::Sync(MegaClient* cclient, string* crootpath, const char* cdebris,
         localdebris = *clocaldebris;
 
         // FIXME: pass last segment of localdebris
-        dirnotify = client->fsaccess->newdirnotify(crootpath, &localdebris);
+        dirnotify = auto_ptr<DirNotify>(client->fsaccess->newdirnotify(crootpath, &localdebris));
     }
 
     // set specified fsfp or get from fs if none
@@ -126,7 +126,6 @@ Sync::~Sync()
     }
 
     delete statecachetable;
-    delete dirnotify;
 
     client->syncs.erase(sync_it);
     client->syncactivity = true;
