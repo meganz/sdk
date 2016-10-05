6a675051ce3da33759540de6a4e40f92ccb4c7fd
Javier Serrano <js@mega.co.nz>
2016-10-05 18:40:39 +0200

    Add the build of WebRTC to iOS build scripts

--- bindings/ios/3rdparty/build-all.sh
+++ bindings/ios/3rdparty/build-all.sh
@@ -2,6 +2,7 @@
 		
 set -e
 
+# MEGA SDK deps 
 sh build-cryptopp.sh
 sh build-openssl.sh
 sh build-cares.sh
@@ -9,13 +10,13 @@ sh build-curl.sh
 sh build-libuv.sh
 sh build-libsodium.sh
 
-if [ "$1" == "--enable-chat"]; then
-  sh build-expat.sh
-  sh build-libevent2.sh
-  sh build-libws.sh
-  sh build-megachat.sh
-fi
+# MEGAchat deps
+sh build-expat.sh
+sh build-libevent2.sh
+sh build-libws.sh
+sh build-webrtc.sh
 
+sh build-megachat.sh
 
 echo "Done."
 
