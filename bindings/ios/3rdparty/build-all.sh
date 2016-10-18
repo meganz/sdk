494e52294a7cb78d8ad80851aa2e0287bb4ad8bb
Javier Navarro <jn@mega.co.nz>
2016-10-18 12:06:00 +0200

    Build MEGAChat dependencies only when using the argument --enable-chat

--- bindings/ios/3rdparty/build-all.sh
+++ bindings/ios/3rdparty/build-all.sh
@@ -11,12 +11,14 @@ sh build-libuv.sh
 sh build-libsodium.sh
 
 # MEGAchat deps
+if [ "$1" == "--enable-chat"]; then
 sh build-expat.sh
 sh build-libevent2.sh
 sh build-libws.sh
 sh build-webrtc.sh
 
 sh build-megachat.sh
+fi
 
 echo "Done."
 
