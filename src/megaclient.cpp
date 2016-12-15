986a158c69043913f732649213b3cadfe4f222b9
Javier Serrano <js@mega.co.nz>
2016-12-15 14:47:23 +0100

    Change the state of a disabled transfer

--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -10302,6 +10302,7 @@ void MegaClient::setmaxconnections(direction_t d, int num)
             TransferSlot *slot = *it++;
             if (slot->transfer->type == d)
             {
+                slot->transfer->state = TRANSFERSTATE_QUEUED;
                 slot->transfer->bt.arm();
                 slot->transfer->cachedtempurl = slot->tempurl;
                 delete slot;
