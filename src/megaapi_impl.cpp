f02d8391e14577b83e74a4d5ffd2b4ca876e97a8
Pablo <pm@mega.nz>
2017-01-12 20:53:24 +0100

    fixed moved and cp: request now return parent node

--- src/megaapi_impl.cpp
+++ src/megaapi_impl.cpp
@@ -8802,6 +8802,18 @@ void MegaApiImpl::putnodes_result(error e, targettype_t t, NewNode* nn)
 
     delete [] nn;
 
+    if (request->getType() == MegaRequest::TYPE_MOVE || request->getType() == MegaRequest::TYPE_COPY)
+    {
+        //scale to get the handle of the moved/copied node
+        Node *ntmp;
+        if (n)
+        {
+            for (ntmp = n; ((ntmp->parent != NULL) && (ntmp->parent->nodehandle != request->getParentHandle()) ); ntmp = ntmp->parent);
+            if ((ntmp->parent != NULL) && (ntmp->parent->nodehandle == request->getParentHandle()) )
+                h = ntmp->nodehandle;
+        }
+    }
+
     if (request->getType() != MegaRequest::TYPE_MOVE)
     {
         request->setNodeHandle(h);
