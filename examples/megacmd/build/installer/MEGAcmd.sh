6917504d3044e1925e496d7bab351f20e6c1078d
polmr <polmr@users.noreply.github.com>
2017-01-13 14:13:29 +0100

    added exit to close terminal after mega-cmd exits

--- examples/megacmd/build/installer/MEGAcmd.sh
+++ examples/megacmd/build/installer/MEGAcmd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-osascript -e 'tell app "Terminal" to do script "/Applications/MEGAcmd.app/Contents/MacOS/mega-cmd" activate'
+osascript -e 'tell app "Terminal" to do script "/Applications/MEGAcmd.app/Contents/MacOS/mega-cmd;exit" activate'
