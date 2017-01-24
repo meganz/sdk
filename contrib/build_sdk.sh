4a39d493855fa92d3228038eb1290852d9d64390
polmr <polmr@users.noreply.github.com>
2017-01-24 10:25:38 +0100

    fixed build_sdk.sh missing an space

--- contrib/build_sdk.sh
+++ contrib/build_sdk.sh
@@ -56,8 +56,8 @@ on_exit_ok() {
         echo "Successfully configured MEGA SDK!"
     elif [ $download_only -eq 1 ]; then
         echo "Successfully downloaded MEGA SDK dependencies!"
-    elif [ $only_build_dependencies -eq 1]; then
-		echo "Successfully built MEGA SDK dependencies!"
+    elif [ $only_build_dependencies -eq 1 ]; then
+        echo "Successfully built MEGA SDK dependencies!"
     else
         echo "Successfully compiled MEGA SDK!"
     fi
