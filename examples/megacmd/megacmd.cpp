81df456696cce42ab18bc2e5006b32307ca18a33
Pablo Martin <pm@mega.nz>
2017-01-25 13:47:48 +0100

    moved objective c code for macos loading stuff into megacmdplatform.mm

--- examples/megacmd/megacmd.cpp
+++ examples/megacmd/megacmd.cpp
@@ -28,6 +28,8 @@
 #include "comunicationsmanager.h"
 #include "listeners.h"
 
+#include "megacmdplatform.h"
+
 #define USE_VARARGS
 #define PREFER_STDARG
 
@@ -2176,59 +2178,6 @@ int quote_detector(char *line, int index)
 
 #ifdef __MACH__
 
-char *runWithRootPrivileges(char *command)
-{
-    OSStatus status;
-    AuthorizationRef authorizationRef;
-
-    const char *prompt = "MEGAcmd. ";
-
-    char *result = NULL;
-
-    char* args[3];
-    args [0] = "-e";
-    args [1] = command;
-    args [2] = NULL;
-
-    FILE *pipe = NULL;
-
-    AuthorizationItem kAuthEnv[] = {/* { kAuthorizationEnvironmentIcon, strlen(icon), (void*)icon, 0 },*/
-        {kAuthorizationEnvironmentPrompt, strlen(prompt), (char *) prompt, 0}};
-    AuthorizationEnvironment myAuthorizationEnvironment = { 2, kAuthEnv };
-
-    AuthorizationItem right = {kAuthorizationRightExecute, 0, NULL, 0};
-    AuthorizationRights rights = {1, &right};
-    AuthorizationFlags flags = kAuthorizationFlagDefaults |
-    kAuthorizationFlagInteractionAllowed |
-    kAuthorizationFlagPreAuthorize |
-    kAuthorizationFlagExtendRights;
-
-    status = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment,
-                                     kAuthorizationFlagDefaults, &authorizationRef);
-    if (status != errAuthorizationSuccess)
-    {
-        return NULL;
-    }
-
-    // Call AuthorizationCopyRights to determine rights.
-    status = AuthorizationCopyRights(authorizationRef, &rights, &myAuthorizationEnvironment, flags, NULL);
-    if (status != errAuthorizationSuccess)
-    {
-        return NULL;
-    }
-
-    status = AuthorizationExecuteWithPrivileges(authorizationRef, "/usr/bin/osascript",
-                                                kAuthorizationFlagDefaults, args, &pipe);
-    AuthorizationFree(authorizationRef, kAuthorizationFlagDestroyRights);
-    if (status == errAuthorizationSuccess)
-    {
-        result = new char[1024];
-        fread(result, 1024, 1, pipe);
-        fclose(pipe);
-    }
-
-    return result;
-}
 
 bool enableSetuidBit()
 {
