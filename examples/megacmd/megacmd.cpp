5ba6583be0f410f4d0d69ebf73e55e92ea1c72e0
Pablo Martin <pm@mega.nz>
2017-02-01 16:08:52 +0100

    fixed clear for windows

--- examples/megacmd/megacmd.cpp
+++ examples/megacmd/megacmd.cpp
@@ -1833,7 +1833,31 @@ void executecommand(char* ptr)
 
     if ( thecommand == "clear" )
     {
+#ifdef _WIN32
+        HANDLE hStdOut;
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        DWORD count;
+
+        hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
+        if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+        /* Get the number of cells in the current buffer */
+        if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
+        /* Fill the entire buffer with spaces */
+        if (!FillConsoleOutputCharacter( hStdOut, (TCHAR) ' ', csbi.dwSize.X *csbi.dwSize.Y, { 0, 0 }, &count ))
+        {
+            return;
+        }
+        /* Fill the entire buffer with the current colors and attributes */
+        if (!FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.dwSize.X *csbi.dwSize.Y, { 0, 0 }, &count))
+        {
+            return;
+        }
+        /* Move the cursor home */
+        SetConsoleCursorPosition( hStdOut, { 0, 0 } );
+#else
         rl_clear_screen(0,0);
+#endif
         return;
     }
     cmdexecuter->executecommand(words, &clflags, &cloptions);
