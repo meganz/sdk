aef67cfb68730d41496f4401dd3a8dc861b1efd9
Javier Navarro <jn@mega.co.nz>
2017-02-21 12:51:14 +0100

    Update script build-curl.sh (iOS)

--- bindings/ios/3rdparty/build-curl.sh
+++ bindings/ios/3rdparty/build-curl.sh
@@ -71,6 +71,8 @@ else
 ./configure --host=${ARCH}-apple-darwin --enable-static --disable-shared --with-ssl=${OPENSSL_PREFIX} --with-zlib --disable-manual --disable-ftp --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-proxy --disable-dict --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smtp --disable-gopher --disable-sspi --enable-ipv6 --disable-smb
 fi
 
+sed -i '' $'s/\#define HAVE_CLOCK_GETTIME_MONOTONIC 1/\/* \#undef HAVE_CLOCK_GETTIME_MONOTONIC 1 *\//' lib/curl_config.h
+
 make -j8
 cp -f lib/.libs/libcurl.a ${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/
 #make clean
