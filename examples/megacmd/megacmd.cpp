6b9af07aae2ec45e0a4f8e5d55e8f2463b551472
Pablo Martin <pm@mega.nz>
2017-05-25 18:51:01 +0200

    implemented custom rl_getc_function to allow inputting unicode chars

--- examples/megacmd/megacmd.cpp
+++ examples/megacmd/megacmd.cpp
@@ -165,7 +165,11 @@ vector<string> emailpatterncommands(aemailpatterncommands, aemailpatterncommands
 string avalidCommands [] = { "login", "signup", "confirm", "session", "mount", "ls", "cd", "log", "debug", "pwd", "lcd", "lpwd", "import",
                              "put", "get", "attr", "userattr", "mkdir", "rm", "du", "mv", "cp", "sync", "export", "share", "invite", "ipc",
                              "showpcr", "users", "speedlimit", "killsession", "whoami", "help", "passwd", "reload", "logout", "version", "quit",
-                             "history", "thumbnail", "preview", "find", "completion", "clear", "https", "transfers"};
+                             "history", "thumbnail", "preview", "find", "completion", "clear", "https", "transfers"
+#ifdef _WIN32
+                             ,"unicode"
+#endif
+                           };
 vector<string> validCommands(avalidCommands, avalidCommands + sizeof avalidCommands / sizeof avalidCommands[0]);
 
 
@@ -309,8 +313,10 @@ static void store_line(char* l)
 {
     if (!l)
     {
+#ifndef _WIN32 // to prevent exit with Supr key
         doExit = true;
         rl_set_prompt("(CTRL+D) Exiting ...\n");
+#endif
         return;
     }
 
@@ -1116,6 +1122,10 @@ const char * getUsageStr(const char *command)
     {
         return "mount";
     }
+    if (!strcmp(command, "unicode"))
+    {
+        return "unicode";
+    }
     if (!strcmp(command, "ls"))
     {
         return "ls [-lRr] [remotepath]";
@@ -1407,6 +1417,14 @@ string getHelpStr(const char *command)
     else if (!strcmp(command, "mount"))
     {
         os << "Lists all the main nodes" << endl;
+    }
+    else if (!strcmp(command, "unicode"))
+    {
+        os << "Toogle unicode input enabled/disabled in interactive shell" << endl;
+        os << endl;
+        os << " Unicode mode is experimental, you might experience" << endl;
+        os << " some issues interacting with the console" << endl;
+        os << " (e.g. history navigation fails)" << endl;
     }
     else if (!strcmp(command, "ls"))
     {
@@ -1844,6 +1862,75 @@ void printAvailableCommands(int extensive = 0)
     }
 }
 
+
+#ifdef _WIN32
+//#include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
+
+/**
+ * @brief getcharacterreadlineUTF16support
+ * while this works, somehow arrows and other readline stuff is disabled using this one.
+ * @param stream
+ * @return
+ */
+int getcharacterreadlineUTF16support (FILE *stream)
+{
+
+    int result;
+    wchar_t c;
+    char b[10];
+    memset(b,0,10);
+
+    while (1)
+    {
+        int oldmode = _setmode(fileno(stream), _O_U16TEXT);
+
+        result = read (fileno (stream), &b, 10);
+        _setmode(fileno(stream), oldmode);
+
+        if (result == 0)
+        {
+            return (EOF);
+        }
+
+        // convert the UTF16 string to widechar
+        size_t wbuffer_size;
+        mbstowcs_s(&wbuffer_size, NULL, 0, b, _TRUNCATE);
+        wchar_t *wbuffer = new wchar_t[wbuffer_size];
+        mbstowcs_s(&wbuffer_size, wbuffer, wbuffer_size, b, _TRUNCATE);
+
+        // convert the UTF16 widechar to UTF8 string
+        string receivedutf8;
+        MegaApi::utf16ToUtf8(wbuffer, wbuffer_size,&receivedutf8);
+
+        if (strlen(receivedutf8.c_str()) > 1) //multi byte utf8 sequence: place the UTF8 characters into rl buffer one by one
+        {
+            for (u_int i=0;i< strlen(receivedutf8.c_str());i++)
+            {
+                rl_line_buffer[rl_end++] = receivedutf8.c_str()[i];
+                rl_point=rl_end;
+            }
+            rl_line_buffer[rl_end] = '\0';
+
+            return 0;
+        }
+
+        if (result =! 0)
+        {
+            return (b[0]);
+        }
+
+        /* If zero characters are returned, then the file that we are
+     reading from is empty!  Return EOF in that case. */
+        if (result == 0)
+        {
+            return (EOF);
+        }
+    }
+}
+#endif
+
 void executecommand(char* ptr)
 {
     vector<string> words = getlistOfWords(ptr);
@@ -2032,6 +2119,14 @@ void executecommand(char* ptr)
 #endif
         return;
     }
+#ifdef _WIN32
+    if (words[0] == "unicode")
+    {
+        rl_getc_function=(rl_getc_function==&getcharacterreadlineUTF16support)?rl_getc:&getcharacterreadlineUTF16support;
+        OUTSTREAM << "Unicode input " << ((rl_getc_function==&getcharacterreadlineUTF16support)?"ENABLED":"DISABLED") << endl;
+        return;
+    }
+#endif
     cmdexecuter->executecommand(words, &clflags, &cloptions);
 }
 
