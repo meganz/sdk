44d14a35e23702c88534377de0d89e5a0bb6e55c
Javier Gomez <jg@mega.co.nz>
2017-08-29 13:56:51 +0200

    Password reminder (WP & UWP bindings)

--- bindings/wp8/MegaSDK.cpp
+++ bindings/wp8/MegaSDK.cpp
@@ -2092,6 +2092,16 @@ String^ MegaSDK::exportMasterKey()
 	return ref new String((wchar_t *)utf16key.data());
 }
 
+void MegaSDK::masterKeyExported(MRequestListenerInterface^ listener)
+{
+    megaApi->masterKeyExported(createDelegateMRequestListener(listener));
+}
+
+void MegaSDK::masterKeyExported()
+{
+    megaApi->masterKeyExported();
+}
+
 void MegaSDK::changePassword(String^ oldPassword, String^ newPassword, MRequestListenerInterface^ listener)
 {
 	std::string utf8oldPassword;
