b781a6a6b98bc7bfd182211902b365d65ab1f122
Javier Serrano <js@mega.co.nz>
2017-10-02 18:20:36 +0200

    Documentation

--- include/megaapi.h
+++ include/megaapi.h
@@ -4208,6 +4208,16 @@ class MegaGlobalListener
          *   Valid data in the MegaEvent object received in the callback:
          *      - MegaEvent::getText: email address used to confirm the account
          *
+         *  - MegaEvent::EVENT_CHANGE_TO_HTTPS: when the SDK automatically starts using HTTPS for all
+         * its communications. This happens when the SDK is able to detect that MEGA servers can't be
+         * reached using HTTP or that HTTP communications are being tampered. Transfers of files and
+         * file attributes (thumbnails and previews) use HTTP by default to save CPU usage. Since all data
+         * is already end-to-end encrypted, it's only needed to use HTTPS if HTTP doesn't work. Anyway,
+         * applications can force the SDK to always use HTTPS using MegaApi::useHttpsOnly. It's recommended
+         * that applications that receive one of these events save that information on its settings and
+         * automatically enable HTTPS on next executions of the app to not force the SDK to detect the problem
+         * and automatically switch to HTTPS every time that the application starts.
+         *
          * You can check the type of event by calling MegaEvent::getType
          *
          * The SDK retains the ownership of the details of the event (\c event).
@@ -4520,6 +4530,16 @@ class MegaListener
          *
          *   Valid data in the MegaEvent object received in the callback:
          *      - MegaEvent::getText: email address used to confirm the account
+         *
+         *  - MegaEvent::EVENT_CHANGE_TO_HTTPS: when the SDK automatically starts using HTTPS for all
+         * its communications. This happens when the SDK is able to detect that MEGA servers can't be
+         * reached using HTTP or that HTTP communications are being tampered. Transfers of files and
+         * file attributes (thumbnails and previews) use HTTP by default to save CPU usage. Since all data
+         * is already end-to-end encrypted, it's only needed to use HTTPS if HTTP doesn't work. Anyway,
+         * applications can force the SDK to always use HTTPS using MegaApi::useHttpsOnly. It's recommended
+         * that applications that receive one of these events save that information on its settings and
+         * automatically enable HTTPS on next executions of the app to not force the SDK to detect the problem
+         * and automatically switch to HTTPS every time that the application starts.
          *
          * You can check the type of event by calling MegaEvent::getType
          *
