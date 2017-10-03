65bfa095bddb7727b4543aa54e59c90ca120407e
Javier Trujillo <jth@mega.nz>
2017-10-03 19:15:19 +0200

    Added support to EventChangeToHttps on Objective-C bindings

--- bindings/ios/Private/DelegateMEGAListener.mm
+++ bindings/ios/Private/DelegateMEGAListener.mm
@@ -197,22 +197,15 @@ void DelegateMEGAListener::onReloadNeeded(MegaApi *api) {
 
 
 void DelegateMEGAListener::onEvent(mega::MegaApi *api, mega::MegaEvent *event) {
-    switch (event->getType()) {
-        case MegaEvent::EVENT_ACCOUNT_CONFIRMATION:            
-            if (listener != nil && [listener respondsToSelector:@selector(onEvent:event:)]) {
-                MegaEvent *tempEvent = NULL;
-                if(event) {
-                    tempEvent = event->copy();
-                }
-                MEGASdk *tempMegaSDK = this->megaSDK;
-                id<MEGADelegate> tempListener = this->listener;
-                dispatch_async(dispatch_get_main_queue(), ^{
-                    [tempListener onEvent:tempMegaSDK event:(tempEvent ? [[MEGAEvent alloc] initWithMegaEvent:tempEvent cMemoryOwn:YES] : nil)];
-                });
-            }
-            break;
-            
-        default:
-            break;
+    if (listener != nil && [listener respondsToSelector:@selector(onEvent:event:)]) {
+        MegaEvent *tempEvent = NULL;
+        if(event) {
+            tempEvent = event->copy();
+        }
+        MEGASdk *tempMegaSDK = this->megaSDK;
+        id<MEGADelegate> tempListener = this->listener;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [tempListener onEvent:tempMegaSDK event:(tempEvent ? [[MEGAEvent alloc] initWithMegaEvent:tempEvent cMemoryOwn:YES] : nil)];
+        });
     }
 }
