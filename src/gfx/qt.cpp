b860ec8c7f1816d3a083b69f7975667516fc73ae
Javier Serrano <js@mega.co.nz>
2018-01-08 05:44:36 +0100

    Simplify the code

--- src/gfx/qt.cpp
+++ src/gfx/qt.cpp
@@ -711,9 +711,10 @@ QImageReader *GfxProcQT::readbitmapFfmpeg(int &w, int &h, int &orientation, QStr
 
     // Calculation of seeking point. We need to rescale time units (seconds) to AVStream.time_base units to perform the seeking
     // Timestamp in streams are measured in frames rather than seconds
+    //int64_t frametimestamp = (int64_t)(5 * AV_TIME_BASE);  // Seek five seconds from the beginning
+    //int64_t seek_target = av_rescale_q(frametimestamp, av_get_time_base_q(), videoStream->time_base);
 
-    int64_t frametimestamp = (int64_t)(5 * AV_TIME_BASE);  // Seek five seconds from the beginning
-    int64_t seek_target = av_rescale_q(frametimestamp, av_get_time_base_q(), videoStream->time_base);
+    int64_t seek_target = videoStream->duration / 5; // 20% of the duration, like the webclient
     if (av_seek_frame(formatContext, videoStreamIdx, seek_target, AVSEEK_FLAG_BACKWARD) < 0)
     {
         LOG_warn << "Error seeking video";
@@ -736,14 +737,8 @@ QImageReader *GfxProcQT::readbitmapFfmpeg(int &w, int &h, int &orientation, QStr
     int actualNumFrames = 0;
     int frameExtracted  = 0;
 
-    unsigned int streamFPS = av_q2d(formatContext->streams[videoStreamIdx]->avg_frame_rate);
-    if (!streamFPS) //If no fps or invalid, set 30 to default
-    {
-        streamFPS = 30;
-    }
-
-    // Read frames until succesfull decodification or reach limit of 7 second read
-    while (av_read_frame(formatContext, &packet) >= 0 && actualNumFrames < (streamFPS * 7))
+    // Read frames until succesfull decodification or reach limit of 220 frames
+    while (av_read_frame(formatContext, &packet) >= 0 && actualNumFrames < 220)
     {
        if (packet.stream_index == videoStream->index)
        {
