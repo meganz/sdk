587b054e0987e42442fb8f6ad41950b96cade56c
Pablo Martin <pm@mega.nz>
2018-03-20 14:32:07 +0100

    add missing namespace

--- include/megaapi_impl.h
+++ include/megaapi_impl.h
@@ -2472,9 +2472,9 @@ public:
     bool useTLS;
     MegaFileSystemAccess *fsAccess;
 
-    string basePath;
+    std::string basePath;
 
-    MegaHTTPServer(MegaApiImpl *megaApi, string basePath, bool useTLS = false, std::string certificatepath = std::string(), std::string keypath = std::string());
+    MegaHTTPServer(MegaApiImpl *megaApi, std::string basePath, bool useTLS = false, std::string certificatepath = std::string(), std::string keypath = std::string());
     virtual ~MegaHTTPServer();
     bool start(int port, bool localOnly = true);
     void stop();
