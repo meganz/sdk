93e208fc49c81fcda51360d17134c224fd0f654a
Pablo Martin <pm@mega.nz>
2018-11-16 13:40:07 +0100

    do not compile autocomplete for mingw nor android

--- include/mega/autocomplete.h
+++ include/mega/autocomplete.h
@@ -19,7 +19,7 @@
 * program.
 */
 
-#if (__cplusplus >= 201100L) || (defined(_MSC_VER) && _MSC_VER >= 1600)
+#if !defined(__MINGW32__) && !defined(__ANDROID__) && ( (__cplusplus >= 201100L) || (defined(_MSC_VER) && _MSC_VER >= 1600) )
 // autocomplete for clients using c++11 - so far just megacli and megaclc on windows and linux.
 
 #ifndef MEGA_AUTOCOMPLETE_H
