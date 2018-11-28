eb89cd38df7e103f754fa4956181b6bc76f33e40
Matt Weir <mattw@mega.co.nz>
2018-11-28 18:15:16 +1300

    DeletedShare UserAlert indication of type
    indicates whether another user left the share,
    or if access to it was removed.

--- src/megaapi_impl.cpp
+++ src/megaapi_impl.cpp
@@ -1739,6 +1739,9 @@ MegaUserAlertPrivate::MegaUserAlertPrivate(UserAlert::Base *b, MegaClient* mc)
         nodePath = p->folderPath;
         nodeName = p->folderName;
         nodeHandle = p->folderHandle;
+        bool accessRevoked = p->userHandle == p->ownerHandle;
+        numbers.push_back(accessRevoked ? 1 : 0);
+        extraStrings.push_back(accessRevoked ? "" : p->removerEmail);
     }
     break;
     case UserAlert::type_put:
