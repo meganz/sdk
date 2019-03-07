8d84ef0f0439e602765c132cb0ea76e0e987458b
Matt Weir <mattw@mega.co.nz>
2019-03-07 18:25:16 +1300

    right-size the attribute string
    using reserve() since putfa needs the right size() on the string

--- src/megaapi_impl.cpp
+++ src/megaapi_impl.cpp
@@ -17949,8 +17949,9 @@ void MegaApiImpl::sendPendingRequests()
                 break;
             }
 
-            // make the string a little bit larger initially with SymmCipher::BLOCKSIZE to avoid growing it for the encryption
-            if(!f->fread(attributedata, unsigned(f->size), SymmCipher::BLOCKSIZE, 0))
+            // make the string a little bit larger initially with SymmCipher::BLOCKSIZE to avoid heap activity growing it for the encryption
+            attributedata->reserve(f->size + SymmCipher::BLOCKSIZE);
+            if(!f->fread(attributedata, unsigned(f->size), 0, 0))
             {
                 delete f;
                 delete attributedata;
