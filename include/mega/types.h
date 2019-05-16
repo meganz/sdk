cee72f105b89d5c1aefb411b7d7d4e3e20bd9bba
Matt Weir <mattw@mega.co.nz>
2019-05-16 12:34:42 +1200

    fix build issues after merge

--- include/mega/types.h
+++ include/mega/types.h
@@ -438,8 +438,8 @@ typedef enum {
     ATTR_STORAGE_STATE = 21,                // private - non-encrypted - char array in B64 - non-versioned
     ATTR_GEOLOCATION = 22,                  // private - byte array - non-versioned
     ATTR_CAMERA_UPLOADS_FOLDER = 23,        // private - byte array - non-versioned
-    ATTR_MY_CHAT_FILES_FOLDER = 24          // private - byte array - non-versioned
-    ATTR_UNSHAREABLE_ATTR = 25,             // private - char array
+    ATTR_MY_CHAT_FILES_FOLDER = 24,         // private - byte array - non-versioned
+    ATTR_UNSHAREABLE_ATTR = 25              // private - char array
 
 } attr_t;
 typedef map<attr_t, string> userattr_map;
--- tests/sdk_test.h
+++ tests/sdk_test.h
@@ -127,7 +127,7 @@ protected:
     void onContactRequestsUpdate(MegaApi* api, MegaContactRequestList* requests) override;
     void onReloadNeeded(MegaApi *api) override {}
 #ifdef ENABLE_SYNC
-    void onSyncFileStateChanged(MegaApi *api, MegaSync *sync, const char *filePath, int newState) override {}
+    void onSyncFileStateChanged(MegaApi *api, MegaSync *sync, string* filePath, int newState) override {}
     void onSyncEvent(MegaApi *api, MegaSync *sync,  MegaSyncEvent *event) override {}
     void onSyncStateChanged(MegaApi *api,  MegaSync *sync) override {}
     void onGlobalSyncStateChanged(MegaApi* api) override {}
