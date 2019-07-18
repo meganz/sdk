5cdd36ed3aa5f5ac85cae05cec0375abbb7a6bb0
Carlos Martín <cma@mega.nz>
2019-07-18 17:32:06 +0200

    Update bindings/ios/MEGARequest.mm
    Code review
    
    Co-Authored-By: Simon Wang <43154373+simonwng@users.noreply.github.com>

--- bindings/ios/MEGARequest.mm
+++ bindings/ios/MEGARequest.mm
@@ -210,7 +210,7 @@ using namespace mega;
 
 - (NSArray<MEGAStringList *> *)megaStringTableArray {
     MegaStringTable *table = self.megaRequest ? self.megaRequest->getMegaStringTable() : nil;
-    NSMutableArray<MEGAStringList *> *array = NSMutableArray.alloc.init;
+    NSMutableArray<MEGAStringList *> *array = [NSMutableArray arrayWithCapacity:table->size()];
     for (int i = 0; i < table->size(); i++) {
         [array addObject:[[MEGAStringList alloc] initWithMegaStringList:(MegaStringList *)table->get(i) cMemoryOwn:YES]];
     }
