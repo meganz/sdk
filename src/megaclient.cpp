8c67f2ed574a7b134aa14803ace9ccda35dc88d4
Christian Blume <cb@mega.nz>
2019-07-31 13:53:13 +1200

    Add missing 'else' keyword

--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -11383,6 +11383,7 @@ error MegaClient::addsync(string* rootpath, const char* debris, string* localdeb
                 {
                     LOG_info << "Successfully assigned fs IDs for filesystem with unstable IDs";
                 }
+                else
                 {
                     LOG_warn << "Failed to assign fs IDs for filesystem with unstable IDs";
                 }
