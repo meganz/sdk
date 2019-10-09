881dbf402e30ead0caec6034f653f524740e321a
Javier Gonzalez Andres <jga@mega.nz>
2019-10-09 14:16:38 +0200

    Revert "Adjustments in node sorting"
    This reverts commit 48b1226bb1227e8d0511b2f853b8364a9850182f.

--- src/megaapi_impl.cpp
+++ src/megaapi_impl.cpp
@@ -16492,7 +16492,7 @@ bool MegaApiImpl::nodeComparatorSizeDESC(Node *i, Node *j)
     }
     if (i->type != FILENODE) // Only file nodes have size
     {
-        return nodeNaturalComparatorASC(i, j);
+        return nodeNaturalComparatorDESC(i, j);
     }
 
     m_off_t r = i->size - j->size;
@@ -16536,7 +16536,7 @@ bool MegaApiImpl::nodeComparatorCreationDESC(Node *i, Node *j)
     {
         return 1;
     }
-    return nodeNaturalComparatorASC(i, j);
+    return nodeNaturalComparatorDESC(i, j);
 }
 
 bool MegaApiImpl::nodeComparatorModificationASC(Node *i, Node *j)
