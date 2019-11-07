c41154545077126bc9f8e4633d9ba499673842da
Sergio Hernández <shs@mega.nz>
2019-11-07 20:28:16 +0100

    Flag for B64 encoding of message

--- src/commands.cpp
+++ src/commands.cpp
@@ -5312,6 +5312,7 @@ CommandSupportTicket::CommandSupportTicket(MegaClient *client, const char *messa
 {
     cmd("sse");
     arg("t", type);
+    arg("b", 1);    // base64 encoding for `msg`
     arg("m", (const byte*)message, int(strlen(message)));
 
     tag = client->reqtag;
