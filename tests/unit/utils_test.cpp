b4b4f11eaf5626dd2e0483379ac074bd1006240d
Christian Blume <cb@mega.nz>
2019-11-11 12:16:32 +1300

    Fix hashCombine unit test

--- tests/unit/utils_test.cpp
+++ tests/unit/utils_test.cpp
@@ -27,5 +27,10 @@ TEST(utils, hashCombine_integer)
 {
     size_t hash = 0;
     mega::hashCombine(hash, 42);
-    ASSERT_EQ(sizeof(hash) == 4 ? 286246808u : 2654435811u, hash);
+#ifdef _WIN32
+    // MSVC's std::hash gives different values than that of gcc/clang
+    ASSERT_EQ(sizeof(hash) == 4 ? 286246808ul : 10203658983813110072ull, hash);
+#else
+    ASSERT_EQ(2654435811ull, hash);
+#endif
 }
