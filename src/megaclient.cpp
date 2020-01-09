fa5d3cca9d2f65b26725e80e3ff80817bdfea614
Christian Blume <cb@mega.nz>
2020-01-09 16:37:37 +1300

    Add logging for when sctable access fails

--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -4525,6 +4525,7 @@ void MegaClient::updatesc()
                         LOG_verbose << "Removing node from database: " << (Base64::btoa((byte*)&((*it)->nodehandle),MegaClient::NODEHANDLE,base64) ? base64 : "");
                         if (!(complete = sctable->del((*it)->dbid)))
                         {
+                            LOG_err << "Failed to remove node: " << (base64 ? base64 : "");
                             break;
                         }
                     }
@@ -4534,6 +4535,7 @@ void MegaClient::updatesc()
                     LOG_verbose << "Adding node to database: " << (Base64::btoa((byte*)&((*it)->nodehandle),MegaClient::NODEHANDLE,base64) ? base64 : "");
                     if (!(complete = sctable->put(CACHEDNODE, *it, &key)))
                     {
+                        LOG_err << "Failed to add node: " << (base64 ? base64 : "");
                         break;
                     }
                 }
