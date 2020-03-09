566b3afda02ad0296f4fecc578c0f79886f457d7
Matt Weir <mattw@mega.co.nz>
2020-03-10 12:59:45 +1300

    timeout needed for transfer completion, in case we do not get a callback

--- tests/integration/SdkTest_test.h
+++ tests/integration/SdkTest_test.h
@@ -79,9 +79,14 @@ struct TransferTracker : public ::mega::MegaTransferListener
         finished = true;
         promiseResult.set_value(result);
     }
-    int waitForResult()
+    int waitForResult(int seconds)
     {
-        return promiseResult.get_future().get();
+        auto& f = promiseResult.get_future();
+        if (std::future_status::ready != f.wait_for(std::chrono::seconds(seconds)))
+        {
+            return -999; // local timeout
+        }
+        return f.get();
     }
 };
 
