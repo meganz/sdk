b2c252b13d6ca9ca1b49878d2024db57c578c755
Sergio Hernández <shs@mega.nz>
2020-03-11 12:37:26 +0100

    Apply same timeout to RequestTracker

--- tests/integration/SdkTest_test.h
+++ tests/integration/SdkTest_test.h
@@ -106,9 +106,14 @@ struct RequestTracker : public ::mega::MegaRequestListener
         finished = true;
         promiseResult.set_value(result);
     }
-    int waitForResult()
+    int waitForResult(int seconds = maxTimeout)
     {
-        return promiseResult.get_future().get();
+        auto f = promiseResult.get_future();
+        if (std::future_status::ready != f.wait_for(std::chrono::seconds(seconds)))
+        {
+            return -999; // local timeout
+        }
+        return f.get();
     }
 };
 
