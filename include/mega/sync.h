c2a06239b0dcd8d06009b3e663e91a1fb714d886
Matt Weir <mattw@mega.co.nz>
2020-03-26 13:17:36 +1300

    Small comment tidy up after develop was merged

--- include/mega/sync.h
+++ include/mega/sync.h
@@ -131,7 +131,7 @@ public:
     void deletemissing(LocalNode*);
 
     // scan specific path
-    LocalNode* checkpath(LocalNode*, string*, string* /*= NULL*/, dstime* /*= NULL*/, bool wejustcreatedthisfolder /*= false*/, DirAccess* iteratingDir /* = nullptr*/);
+    LocalNode* checkpath(LocalNode*, string*, string*, dstime*, bool wejustcreatedthisfolder, DirAccess* iteratingDir);
 
     m_off_t localbytes = 0;
     unsigned localnodes[2]{};
