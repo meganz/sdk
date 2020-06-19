fda05a2e7d4b1fe2c5be722db7a42d94e9d5fcf0
Pablo Martin <pm@mega.nz>
2020-06-19 09:17:47 +0200

    minor improvement in assert

--- src/transfer.cpp
+++ src/transfer.cpp
@@ -387,7 +387,7 @@ void Transfer::failed(error e, DBTableTransactionCommitter& committer, dstime ti
 
     if (e == API_EOVERQUOTA)
     {
-        assert(type != PUT || !timeleft); //only expected overstorage overquota for uploads
+        assert((type == PUT && !timeleft) || (type == GET && timeleft)); // overstorage only possible for uploads, overbandwidth for downloads
         if (!slot)
         {
             bt.backoff(timeleft ? timeleft : NEVER);
