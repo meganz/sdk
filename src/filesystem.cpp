376e03791d9e84135e3f07a56e874c3255656dc2
Javier Gonzalez Andres <jga@mega.nz>
2020-07-16 17:39:50 +0200

    Remove unnecessary macro

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -151,7 +151,7 @@ FileSystemType FileSystemAccess::getlocalfstype(const string *dstPath) const
             return FS_FAT32;
         }
     }
-#elif defined(_WIN32) || defined(_WIN64) || defined(WINDOWS_PHONE)
+#elif defined(_WIN32) || defined(WINDOWS_PHONE)
     // Filesystem detection for Windows
     std::wstring wPath(dstPath->begin(), dstPath->end());
     std::wstring volMountPoint;
@@ -341,7 +341,7 @@ const char *FileSystemAccess::getPathSeparator()
 {
 #if defined (__linux__) || defined (__ANDROID__) || defined  (__APPLE__) || defined (USE_IOS)
 return "/";
-#elif defined(_WIN32) || defined(_WIN64) || defined(WINDOWS_PHONE)
+#elif defined(_WIN32) || defined(WINDOWS_PHONE)
 return "\\";
 #elif
 // Default case
