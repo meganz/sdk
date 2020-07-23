8769120401ddb8a402986fddeb19ac2334acda54
matt weir <mattw@mega.co.nz>
2020-07-23 14:03:10 +1200

    For the MassNotifyFromLocalFolderTree test, wait for notifications to finish processing
    On a slower machine, it may take longer, the main thing is that we do process them all eventually, without losing any
    So we check the queue lengths periodically, waiting until they are all finished, with an overall timeout of 5 minutes.
    Also fixed a shutdown issue where destroying a Sync was slow unless we were also destroying the client, which is the
    case for this test, so adjusted that optimization to apply to ~Sync.

--- src/node.cpp
+++ src/node.cpp
@@ -1211,7 +1211,7 @@ void LocalNode::setnameparent(LocalNode* newparent, LocalPath* newlocalpath, std
         }
     }
 
-    if (parent && parent != newparent && !sync->client->destructorRunning)
+    if (parent && parent != newparent && !sync->destructorRunning)
     {
         treestate(TREESTATE_NONE);
     }
--- src/sync.cpp
+++ src/sync.cpp
@@ -762,6 +762,7 @@ Sync::~Sync()
 {
     // must be set to prevent remote mass deletion while rootlocal destructor runs
     assert(state == SYNC_CANCELED || state == SYNC_FAILED);
+    destructorRunning = true;
 
     if (!statecachetable && client->syncConfigs)
     {
