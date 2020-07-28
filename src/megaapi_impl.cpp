0af8951c7c8d74f85ee027015dcd16e8663d02d7
Javier Gonzalez Andres <jga@mega.nz>
2020-07-28 11:42:57 +0200

    Retrieve lbl/fav for new version for request TYPE_COMPLETE_BACKGROUND_UPLOAD

--- src/megaapi_impl.cpp
+++ src/megaapi_impl.cpp
@@ -22008,6 +22008,25 @@ void MegaApiImpl::sendPendingRequests()
             }
 #endif
             AttrMap attrs;
+            Node *previousNode = client->childnodebyname(parentNode, utf8Name, true);
+            if (!client->versions_disabled && previousNode)
+            {
+               assert(previousNode->type == FILENODE);
+               nameid favnid = AttrMap::string2nameid("fav");
+               auto it = previousNode->attrs.map.find(favnid);
+               if (it != previousNode->attrs.map.end())
+               {
+                   attrs.map[favnid] = it->second;
+               }
+
+               nameid lblnid = AttrMap::string2nameid("lbl");
+               it = previousNode->attrs.map.find(lblnid);
+               if (it != previousNode->attrs.map.end())
+               {
+                   attrs.map[lblnid] = it->second;
+               }
+            }
+
             attrs.map['n'] = utf8Name;
             attrs.map['c'] = megafingerprint.get();
             if (fingerprintOriginal)
