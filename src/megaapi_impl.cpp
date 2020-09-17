c1f154766f3f14fe36aa10e151e64e452a9665be
Javier Navarro <jn@mega.co.nz>
2020-09-17 18:19:39 +0200

    Get mega dns servers from ns.mega.co.nz on iOS
    (IOS-4179)

--- src/megaapi_impl.cpp
+++ src/megaapi_impl.cpp
@@ -6213,6 +6213,8 @@ void MegaApiImpl::loop()
     {
     #ifdef WINDOWS_PHONE
         client->httpio->getMEGADNSservers(&servers, false);
+    #elif TARGET_OS_IPHONE
+        client->httpio->getMEGADNSservers(&servers);
     #else
         __res_state res;
         bool valid;
@@ -19854,54 +19856,6 @@ void MegaApiImpl::sendPendingRequests()
                 {
                     servers = dnsservers;
                 }
-#if TARGET_OS_IPHONE
-                else
-                {
-                    // Workaround to get the IP of valid DNS servers on iOS
-                    __res_state res;
-                    bool valid;
-                    if (res_ninit(&res) == 0)
-                    {
-                        union res_sockaddr_union u[MAXNS];
-                        int nscount = res_getservers(&res, u, MAXNS);
-
-                        for (int i = 0; i < nscount; i++)
-                        {
-                            char straddr[INET6_ADDRSTRLEN];
-                            straddr[0] = 0;
-                            valid = false;
-
-                            if (u[i].sin.sin_family == PF_INET)
-                            {
-                                valid = mega_inet_ntop(PF_INET, &u[i].sin.sin_addr, straddr, sizeof(straddr)) == straddr;
-                            }
-
-                            if (u[i].sin6.sin6_family == PF_INET6)
-                            {
-                                valid = mega_inet_ntop(PF_INET6, &u[i].sin6.sin6_addr, straddr, sizeof(straddr)) == straddr;
-                            }
-
-                            if (valid && straddr[0])
-                            {
-                                if (servers.size())
-                                {
-                                    servers.append(",");
-                                }
-                                servers.append(straddr);
-                            }
-                        }
-
-                        res_ndestroy(&res);
-                    }
-
-                    if (!servers.size())
-                    {
-                        LOG_warn << "Failed to get DNS servers at Retry Pending Connections";
-                        e = API_EACCESS;    // ie. when iOS has no Internet connection at all
-                        break;
-                    }
-                }
-#endif
 #ifndef __MINGW32__
                 if (servers.size())
                 {
