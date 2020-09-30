ca72e2acdd0b35c00bfea1e1e9a415b8fdcbdb59
Javier Navarro <jn@mega.co.nz>
2020-09-30 17:47:49 +0200

    Update c-ares to version 1.16.1 (iOS)
    Don't apply the patch different_address.patch
    Remove old arch i386 and armv7

--- bindings/ios/3rdparty/build-cares.sh
+++ bindings/ios/3rdparty/build-cares.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 		
-CARES_VERSION="1.13.0"
+CARES_VERSION="1.16.1"
 SDKVERSION=`xcrun -sdk iphoneos --show-sdk-version`														  
 
 
 ##############################################
 CURRENTPATH=`pwd`
-ARCHS="i386 x86_64 armv7 armv7s arm64"
+ARCHS="x86_64 armv7s arm64"
 DEVELOPER=`xcode-select -print-path`
 
 if [ ! -d "$DEVELOPER" ]; then
@@ -41,11 +41,10 @@ fi
 
 tar zxf c-ares-${CARES_VERSION}.tar.gz
 pushd "c-ares-${CARES_VERSION}"
-patch -p1 < ../different_address.patch
 
 for ARCH in ${ARCHS}
 do
-if [[ "${ARCH}" == "i386" || "${ARCH}" == "x86_64" ]];
+if [ "${ARCH}" == "x86_64" ];
 then
 PLATFORM="iPhoneSimulator"
 else
@@ -60,9 +59,9 @@ export CC="${BUILD_TOOLS}/usr/bin/gcc -arch ${ARCH}"
 mkdir -p "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk"
 
 # Build
-export LDFLAGS="-Os -arch ${ARCH} -Wl,-dead_strip -miphoneos-version-min=7.0 -L${BUILD_SDKROOT}/usr/lib"
-export CFLAGS="-Os -arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${BUILD_SDKROOT} -miphoneos-version-min=7.0"
-export CPPFLAGS="${CFLAGS} -I${BUILD_SDKROOT}/usr/include"
+export LDFLAGS="-Os -arch ${ARCH} -Wl,-dead_strip -miphoneos-version-min=10.0"
+export CFLAGS="-Os -arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${BUILD_SDKROOT} -miphoneos-version-min=10.0"
+export CPPFLAGS="${CFLAGS}"
 export CXXFLAGS="${CPPFLAGS}"
 
 if [ "${ARCH}" == "arm64" ]; then
@@ -70,8 +69,6 @@ if [ "${ARCH}" == "arm64" ]; then
 else
 ./configure --host=${ARCH}-apple-darwin --enable-static --disable-shared
 fi
-
-sed -i '' $'s/\#define HAVE_CLOCK_GETTIME_MONOTONIC 1/\/* \#undef HAVE_CLOCK_GETTIME_MONOTONIC 1 *\//' ares_config.h
 
 make -j8
 cp -f .libs/libcares.a ${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/
@@ -81,12 +78,13 @@ done
 
 popd
 mkdir lib || true
-lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-i386.sdk/libcares.a ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-x86_64.sdk/libcares.a  ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7.sdk/libcares.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7s.sdk/libcares.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-arm64.sdk/libcares.a -output ${CURRENTPATH}/lib/libcares.a
+lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-x86_64.sdk/libcares.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7s.sdk/libcares.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-arm64.sdk/libcares.a -output ${CURRENTPATH}/lib/libcares.a
 mkdir -p include || true
 cp -f c-ares-${CARES_VERSION}/ares.h c-ares-${CARES_VERSION}/ares_build.h c-ares-${CARES_VERSION}/ares_dns.h c-ares-${CARES_VERSION}/ares_rules.h c-ares-${CARES_VERSION}/ares_version.h include/
 
 
-rm -rf c-ares-${CARES_VERSION}
 rm -rf bin
+rm -rf c-ares-${CARES_VERSION}
+rm -rf c-ares-${CARES_VERSION}.tar.gz
 
 echo "Done."
