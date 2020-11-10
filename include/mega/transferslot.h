9d4fb0c437b3d88ba0cdda5201de17332adc7ae7
Matt Weir <mattw@mega.co.nz>
2020-11-10 18:18:37 +1300

    Recheck MAC on download MAC failure, in case it was the result of a mac calculation race
    In rare cases, some uploads were missing the macs of one of the final upload pieces in the mac-of-macs calculation.
    Recognize these and reconfirm the MAC, approve the download.

--- include/mega/transferslot.h
+++ include/mega/transferslot.h
@@ -128,6 +128,7 @@ struct MEGA_API TransferSlot
 
     // compute the meta MAC based on the chunk MACs
     int64_t macsmac(chunkmac_map*);
+    int64_t macsmac_gaps(chunkmac_map*, size_t g1, size_t g2, size_t g3, size_t g4);
 
     // tslots list position
     transferslot_list::iterator slots_it;
@@ -145,7 +146,9 @@ struct MEGA_API TransferSlot
 private:
     void toggleport(HttpReqXfer* req);
     bool tryRaidRecoveryFromHttpGetError(unsigned i, bool incrementErrors);
-    bool checkTransferFinished(DBTableTransactionCommitter& committer, MegaClient* client);
+    bool checkDownloadTransferFinished(DBTableTransactionCommitter& committer, MegaClient* client);
+    bool checkMetaMacWithMissingLateEntries();
+    void updateMacInKey(int64_t correctMac);
 
     // returns true if connection haven't received data recently (set incrementErrors) or if slower than other connections (reset incrementErrors)
     bool testForSlowRaidConnection(unsigned connectionNum, bool& incrementErrors);
