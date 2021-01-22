b54ebc5535f4a68cf45bafc58029a012769d914d
Lia Stratopoulos <77341634+mega-lia@users.noreply.github.com>
2021-01-22 16:11:46 -0500

    add mmacosx-version-min to triplet file

--- contrib/cmake/vcpkg_extra_triplets/x64-osx-mega.cmake
+++ contrib/cmake/vcpkg_extra_triplets/x64-osx-mega.cmake
@@ -13,3 +13,9 @@ else()
     # build this library statically (much simpler installation, debugging, etc)
 	set(VCPKG_LIBRARY_LINKAGE static)
 endif()
+
+set(_macos_version_min_flag "-mmacosx-version-min=${VCPKG_OSX_DEPLOYMENT_TARGET}")
+
+set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} ${_macos_version_min_flag}")
+set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} ${_macos_version_min_flag}")
+set(VCPKG_LINKER_FLAGS "${VCPKG_LINKER_FLAGS} ${_macos_version_min_flag}")
