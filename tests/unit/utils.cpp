df95c0e6375965dd509bde2af13bdf7e0a59dc50
Matt Weir <mattw@mega.co.nz>
2021-03-25 14:24:36 +1300

    fix unit test using 8 random bytes for a 6 byte handle

--- tests/unit/utils.cpp
+++ tests/unit/utils.cpp
@@ -84,7 +84,7 @@ std::unique_ptr<mega::UnifiedSync> makeSync(mega::MegaClient& client, const std:
 {
     mega::FSACCESS_CLASS fsaccess;
     std::string localdebris = gLocalDebris;
-    auto& n = makeNode(client, mega::FOLDERNODE, std::hash<std::string>{}(localname));
+    auto& n = makeNode(client, mega::FOLDERNODE, std::hash<std::string>{}(localname) & 0xFFFFFFFFFFFF);
     auto localdebrisLP = ::mega::LocalPath::fromPath(localdebris, fsaccess);
     mega::SyncConfig config{::mega::LocalPath::fromPath(localname, *client.fsaccess), localname, ::mega::NodeHandle().set6byte(n.nodehandle), std::string(), 0};
 
