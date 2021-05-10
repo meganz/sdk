249abc299cd50ad7a879e9474b18dbc70fff2137
Cătălin Răceanu <cra@mega.nz>
2021-05-11 00:29:34 +0300

    Fix compilation without ffmpeg

--- src/gfx/qt.cpp
+++ src/gfx/qt.cpp
@@ -386,6 +386,7 @@ GfxProcQT::GfxProcQT()
     av_register_all();
     avcodec_register_all();
 //    av_log_set_level(AV_LOG_VERBOSE);
+    }
 #endif
 
 #ifdef HAVE_PDFIUM
@@ -406,7 +407,6 @@ GfxProcQT::GfxProcQT()
     }
 #endif
 #endif
-    }
     image = NULL;
     orientation = -1;
     imageType = TYPE_NONE;
