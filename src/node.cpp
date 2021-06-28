76f6b9e5817b429dd7298b7cca21a1ad1eeefe1e
David Ward <dw@mega.co.nz>
2021-06-28 13:41:09 +1200

    Correct assignment to a file's needsScan flag.

--- src/node.cpp
+++ src/node.cpp
@@ -1652,7 +1652,7 @@ void LocalNode::setSubtreeNeedsRescan(bool includeFiles)
         }
         else
         {
-            child.second->needsRescan = includeFiles;
+            child.second->needsRescan |= includeFiles;
         }
     }
 }
