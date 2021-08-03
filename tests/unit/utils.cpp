5142d8e702fa2d32cacd9afd90b24b3e349c4424
Matt Weir <mattw@mega.co.nz>
2021-08-04 11:01:44 +1200

    fix for unit test build

--- tests/unit/utils.cpp
+++ tests/unit/utils.cpp
@@ -71,7 +71,7 @@ std::shared_ptr<mega::MegaClient> makeClient(mega::MegaApp& app, mega::FileSyste
 
 mega::Node& makeNode(mega::MegaClient& client, const mega::nodetype_t type, const mega::handle handle, mega::Node* const parent)
 {
-    assert(client.nodes.find(handle) == client.nodes.end());
+    assert(client.nodes.find(::mega::NodeHandle().set6byte(handle)) == client.nodes.end());
     mega::node_vector dp;
     const auto ph = parent ? parent->nodehandle : mega::UNDEF;
     auto n = new mega::Node{&client, &dp, handle, ph, type, -1, mega::UNDEF, nullptr, 0}; // owned by the client
