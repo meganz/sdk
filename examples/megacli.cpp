6463278964531cae9115738c3d1949e74e414148
Matt Weir <mattw@mega.co.nz>
2021-08-04 16:00:05 +1200

    Including the corresponding megacli adjustments

--- examples/megacli.cpp
+++ examples/megacli.cpp
@@ -3959,7 +3959,7 @@ void exec_cp(autocomplete::ACState& s)
             if (tn)
             {
                 // add the new nodes
-                client->putnodes(tn->nodehandle, move(tc.nn), nullptr, gNextClientTag++);
+                client->putnodes(tn->nodeHandle(), move(tc.nn), nullptr, gNextClientTag++);
             }
             else
             {
@@ -4300,7 +4300,7 @@ void uploadLocalPath(nodetype_t type, std::string name, LocalPath& localname, No
                 uploadLocalFolderContent(tmp, parent);
             };
 
-            client->putnodes(parent->nodehandle, move(nn), nullptr, gNextClientTag++);
+            client->putnodes(parent->nodeHandle(), move(nn), nullptr, gNextClientTag++);
         }
     }
 }
@@ -4930,7 +4930,7 @@ void exec_mkdir(autocomplete::ACState& s)
             {
                 vector<NewNode> nn(1);
                 client->putnodes_prepareOneFolder(&nn[0], newname);
-                client->putnodes(n->nodehandle, move(nn), nullptr, gNextClientTag++);
+                client->putnodes(n->nodeHandle(), move(nn), nullptr, gNextClientTag++);
             }
             else if (allowDuplicate && n->parent && n->parent->nodehandle != UNDEF)
             {
@@ -4940,7 +4940,7 @@ void exec_mkdir(autocomplete::ACState& s)
                 if (pos != string::npos) leafname.erase(0, pos + 1);
                 vector<NewNode> nn(1);
                 client->putnodes_prepareOneFolder(&nn[0], leafname);
-                client->putnodes(n->parent->nodehandle, move(nn), nullptr, gNextClientTag++);
+                client->putnodes(n->parent->nodeHandle(), move(nn), nullptr, gNextClientTag++);
             }
             else
             {
@@ -7574,7 +7574,7 @@ void DemoApp::openfilelink_result(handle ph, const byte* key, m_off_t size,
             }
         }
 
-        client->putnodes(n->nodehandle, move(nn), nullptr, client->restag);
+        client->putnodes(n->nodeHandle(), move(nn), nullptr, client->restag);
     }
     else
     {
