8b494b3f3773ecb2f31012eaed2bd65f308928da
Javier Navarro <jn@mega.co.nz>
2021-12-16 17:57:34 +0100

    Update webrtc script for iOS
    arg use_xcode_clang doesn't exist use is_clang instead
    
    remove enable_ios_bitcode=true because a compilation issue:
    error: -mllvm is not supported with -fembed-bitcode

--- bindings/ios/3rdparty/build-webrtc.sh
+++ bindings/ios/3rdparty/build-webrtc.sh
@@ -19,15 +19,12 @@ pushd src
 
 git checkout $COMMIT
 
-git apply ../../../../../patches/webRtcPatch.patch
-git apply ../../../../../patches/webRtcSocketIosPatch.patch
-
 mkdir lib
 pushd lib
 
 for ARCH in $ARCHS
 do
-$DEPOT_TOOLS_PATH/gn gen $ARCH --args='target_os="ios" target_cpu="'$ARCH'" rtc_include_tests=false rtc_build_examples=false treat_warnings_as_errors=false fatal_linker_warnings=false use_custom_libcxx=false is_debug=false ios_deployment_target="12.1" rtc_build_tools=false rtc_enable_protobuf=false enable_ios_bitcode=true use_xcode_clang=true is_component_build=false ios_enable_code_signing=false' 
+$DEPOT_TOOLS_PATH/gn gen $ARCH --args='target_os="ios" target_cpu="'$ARCH'" rtc_include_tests=false rtc_build_examples=false treat_warnings_as_errors=false fatal_linker_warnings=false use_custom_libcxx=false is_debug=false ios_deployment_target="13.0" rtc_build_tools=false rtc_enable_protobuf=false is_clang=true is_component_build=false ios_enable_code_signing=false'
 
 pushd $ARCH
 $DEPOT_TOOLS_PATH/ninja -C .
