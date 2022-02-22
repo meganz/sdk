8d07ab2ffeabc93f91a8412c26df51036f956c68
Javier Gonzalez Andres <jga@mega.nz>
2022-02-22 12:52:06 +0100

    Update SDK version

--- include/mega/version.h
+++ include/mega/version.h
@@ -2,8 +2,8 @@
 #define MEGA_MAJOR_VERSION 3
 #endif
 #ifndef MEGA_MINOR_VERSION
-#define MEGA_MINOR_VERSION 9
+#define MEGA_MINOR_VERSION 10
 #endif
 #ifndef MEGA_MICRO_VERSION
-#define MEGA_MICRO_VERSION 12
+#define MEGA_MICRO_VERSION 1
 #endif
