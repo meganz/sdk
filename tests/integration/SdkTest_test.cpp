a70818015d8de8a625ef95dee3f399d7f33a8c74
Sergio Hernández <shs@mega.nz>
2022-04-19 18:49:01 +0200

    Pause transfers in right account
    The account that starts the upload and wants to be paused is the
    `apiIndex=1`, not 0.

--- tests/integration/SdkTest_test.cpp
+++ tests/integration/SdkTest_test.cpp
@@ -7345,7 +7345,7 @@ TEST_F(SdkTest, SdkTargetOverwriteTest)
     megaApi[1]->startUpload(fp.u8string().c_str(), n1);
 
     // --- Pause transfer, revoke out-share permissions for secondary account and resume transfer ---
-    megaApi[0]->pauseTransfers(true);
+    megaApi[1]->pauseTransfers(true);
     ASSERT_TRUE(!mApi[1].transferFlags[MegaTransfer::TYPE_UPLOAD]);
     mApi[0].nodeUpdated = mApi[1].nodeUpdated = false;
     ASSERT_NO_FATAL_FAILURE(shareFolder(n1, mApi[1].email.data(), MegaShare::ACCESS_UNKNOWN));
@@ -7353,7 +7353,7 @@ TEST_F(SdkTest, SdkTargetOverwriteTest)
             << "Node update not received after " << maxTimeout << " seconds";
     ASSERT_TRUE( waitForResponse(&mApi[1].nodeUpdated) )   // at the target side (auxiliar account)
             << "Node update not received after " << maxTimeout << " seconds";
-    megaApi[0]->pauseTransfers(false);
+    megaApi[1]->pauseTransfers(false);
     // --- Wait for transfer completion
     ASSERT_TRUE(waitForResponse(&mApi[1].transferFlags[MegaTransfer::TYPE_UPLOAD], 600))
         << "Upload transfer failed after " << 600 << " seconds";
