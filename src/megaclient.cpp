e4cb0f38d87d49be1cecd1125b0aba263ea05ed6
Cătălin Răceanu <cra@mega.nz>
2022-05-11 17:43:47 +0300

    Refactor discarduser() to avoid duplicated code

--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -10037,9 +10037,8 @@ void MegaClient::mapuser(handle uh, const char* email)
     }
 }
 
-void MegaClient::discarduser(handle uh, bool discardnotified)
+void MegaClient::dodiscarduser(User* u, bool discardnotified)
 {
-    User *u = finduser(uh);
     if (!u)
     {
         return;
@@ -10053,25 +10052,21 @@ void MegaClient::discarduser(handle uh, bool discardnotified)
     }
 
     umindex.erase(u->email);
-    users.erase(uhindex[uh]);
-    uhindex.erase(uh);
+    int uhidx = uhindex[u->userhandle];
+    uhindex.erase(u->userhandle);
+    users.erase(uhidx);
+}
+
+void MegaClient::discarduser(handle uh, bool discardnotified)
+{
+    User *u = finduser(uh);
+    dodiscarduser(u, discardnotified);
 }
 
 void MegaClient::discarduser(const char *email)
 {
     User *u = finduser(email);
-    if (!u)
-    {
-        return;
-    }
-
-    u->removepkrs(this);
-
-    discardnotifieduser(u);
-
-    uhindex.erase(u->userhandle);
-    users.erase(umindex[email]);
-    umindex.erase(email);
+    dodiscarduser(u, true);
 }
 
 PendingContactRequest* MegaClient::findpcr(handle p)
@@ -13311,11 +13306,8 @@ void MegaClient::purgenodesusersabortsc(bool keepOwnUser)
         User *u = &(it->second);
         if ((!keepOwnUser || u->userhandle != me) || u->userhandle == UNDEF)
         {
-            u->removepkrs(this); // especially for HIDDEN users
-            discardnotifieduser(u);
-            umindex.erase(u->email);
-            uhindex.erase(u->userhandle);
-            users.erase(it++);
+            ++it;
+            dodiscarduser(u, true);
         }
         else
         {
