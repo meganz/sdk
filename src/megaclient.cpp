b01695f065c21f62e591cc62dd239bcdc1cfc96b
Sergio Hernández <shs@mega.nz>
2022-05-24 14:53:01 +0200

    Don't set fingerprints for encrypted nodes

--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -17575,6 +17575,13 @@ Node *NodeManager::unserializeNode(const std::string *d, bool fromOldCache)
         LocalPath::utf8_normalize(&(it->second));
     }
 
+    if (!encrypted)
+    {
+        // only if the node is not encrypted, we can generate a valid
+        // fingerprint, based on the node's attribute 'c'
+        n->setfingerprint();
+    }
+
     PublicLink *plink = NULL;
     if (isExported)
     {
@@ -17602,8 +17609,6 @@ Node *NodeManager::unserializeNode(const std::string *d, bool fromOldCache)
         plink = new PublicLink(ph, cts, ets, takendown, authKey ? authKey : "");
     }
     n->plink = plink;
-
-    n->setfingerprint();
 
     if (encrypted)
     {
