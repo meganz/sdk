44088d0aac1df57f3fef2f05e0c82c9ef4b1653a
Javier Navarro <jn@mega.co.nz>
2022-07-11 12:45:00 +0200

    mtime is overriden with 0 when sendPutnodes (Obj-C)

--- bindings/ios/MEGASdk.mm
+++ bindings/ios/MEGASdk.mm
@@ -51,6 +51,8 @@
 
 NSString * const MEGAIsBeingLogoutNotification = @"nz.mega.isBeingLogout";
 
+const int64_t MEGAInvalidCustomModificationTime = -1;
+
 using namespace mega;
 
 @interface MEGASdk () {
@@ -2229,13 +2231,13 @@ using namespace mega;
 
 - (void)startUploadWithLocalPath:(NSString *)localPath parent:(MEGANode *)parent fileName:(nullable NSString *)fileName appData:(nullable NSString *)appData isSourceTemporary:(BOOL)isSourceTemporary startFirst:(BOOL)startFirst cancelToken:(nullable MEGACancelToken *)cancelToken {
     if (self.megaApi) {
-        self.megaApi->startUpload(localPath.UTF8String, parent.getCPtr, fileName.UTF8String, 0, appData.UTF8String, isSourceTemporary, startFirst, cancelToken.getCPtr);
+        self.megaApi->startUpload(localPath.UTF8String, parent.getCPtr, fileName.UTF8String, MEGAInvalidCustomModificationTime, appData.UTF8String, isSourceTemporary, startFirst, cancelToken.getCPtr);
     }
 }
 
 - (void)startUploadWithLocalPath:(NSString *)localPath parent:(MEGANode *)parent fileName:(nullable NSString *)fileName appData:(nullable NSString *)appData isSourceTemporary:(BOOL)isSourceTemporary startFirst:(BOOL)startFirst cancelToken:(nullable MEGACancelToken *)cancelToken delegate:(id<MEGATransferDelegate>)delegate {
     if (self.megaApi) {
-        self.megaApi->startUpload(localPath.UTF8String, parent.getCPtr, fileName.UTF8String, 0, appData.UTF8String, isSourceTemporary, startFirst, cancelToken.getCPtr, [self createDelegateMEGATransferListener:delegate singleListener:YES]);
+        self.megaApi->startUpload(localPath.UTF8String, parent.getCPtr, fileName.UTF8String, MEGAInvalidCustomModificationTime, appData.UTF8String, isSourceTemporary, startFirst, cancelToken.getCPtr, [self createDelegateMEGATransferListener:delegate singleListener:YES]);
     }
 }
 
