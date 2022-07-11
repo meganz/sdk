0f7f85edf80a8c9de8c897b47654f27784277249
David Yuste <dy@mega.nz>
2022-07-11 18:43:54 +0200

    Fix HttpReqCommandPutFA EOO preserving completion callback order

--- src/commands.cpp
+++ src/commands.cpp
@@ -154,13 +154,14 @@ bool HttpReqCommandPutFA::procresult(Result r)
 
                         // cache resolved URLs if received
                         std::vector<string> urls(1, posturl);
-
-                        mCompletion(API_OK, posturl, ips);
+                        std::vector<string> ipsCopy = ips;
 
                         if(!cacheresolvedurls(urls, std::move(ips)))
                         {
                             LOG_err << "Unpaired IPs received for URLs in `ufa` command. URLs: " << urls.size() << " IPs: " << ips.size();
                         }
+
+                        mCompletion(API_OK, posturl, ipsCopy);
 
                         return true;
                     }
