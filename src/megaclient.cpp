36e5d2be9fcd73f9e0b5db5b168e0b49d782866a
Cătălin Răceanu <cra@mega.nz>
2022-08-11 14:06:43 +0300

    Restrict move/rename/delete file for suspended account

--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -8115,6 +8115,11 @@ error MegaClient::checkmove(Node* fn, Node* tn)
 // modify the 'n' attribute)
 error MegaClient::rename(Node* n, Node* p, syncdel_t syncdel, NodeHandle prevparent, const char *newName, CommandMoveNode::Completion&& c)
 {
+    if (mBizStatus <= BIZ_STATUS_INACTIVE)
+    {
+        return API_EACCESS;
+    }
+
     error e;
 
     if ((e = checkmove(n, p)))
@@ -8236,7 +8241,7 @@ void MegaClient::removeOutSharesFromSubtree(Node* n, int tag)
 // delete node tree
 error MegaClient::unlink(Node* n, bool keepversions, int tag, std::function<void(NodeHandle, Error)>&& resultFunction)
 {
-    if (!n->inshare && !checkaccess(n, FULL))
+    if (mBizStatus <= BIZ_STATUS_INACTIVE || (!n->inshare && !checkaccess(n, FULL)))
     {
         return API_EACCESS;
     }
