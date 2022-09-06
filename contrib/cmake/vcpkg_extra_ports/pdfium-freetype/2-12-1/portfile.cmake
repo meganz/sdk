2077fdec2eb0cf3adad29230baea9ed2fb1585fa
Alfonso Abella <aag@mega.co.nz>
2022-09-06 22:03:28 +0200

    Automatically get pdfium using vcpkg instead of depot tools
    Use git to get the minimmum sources
    Remove old unneded scripts
    Upgraded CONTROL files to vcpkg.json
    Enabled pdfium by default

--- contrib/cmake/vcpkg_extra_ports/pdfium-freetype/2-12-1/portfile.cmake
+++ contrib/cmake/vcpkg_extra_ports/pdfium-freetype/2-12-1/portfile.cmake
@@ -1,10 +1,14 @@
-include(vcpkg_common_functions)
+#include(vcpkg_common_functions)
+
+get_filename_component(GIT_PATH ${GIT} DIRECTORY)
+
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://chromium.googlesource.com/chromium/src/third_party/freetype2.git
+    REF 2db58e061ecc0d738a41d13ed8908e967bd0014c # The one in pdfium DEPS file, field 'freetype_revision'
+)
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
-set(_VCPKG_ROOT_DIR "${_VCPKG_ROOT_DIR}${VCPKG_ROOT_DIR}")
-
-set(SOURCE_PATH ${_VCPKG_ROOT_DIR}/pdfium/pdfium/third_party/freetype/src)
 
 vcpkg_configure_cmake(
   SOURCE_PATH ${SOURCE_PATH}
@@ -18,4 +22,4 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/cmake)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/cmake)
 
 file(INSTALL ${SOURCE_PATH}/include DESTINATION "${CURRENT_PACKAGES_DIR}/")
-file(INSTALL ${SOURCE_PATH}/../FTL.TXT DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/" RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/docs/FTL.TXT DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/" RENAME copyright)
