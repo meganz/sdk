02f15e572b6eba01ca4627c2ecbc6856f1d23005
Cătălin Răceanu <cra@mega.nz>
2022-09-07 13:33:04 +0300

    Make expanselocalpath() src argument const

--- include/mega/filesystem.h
+++ include/mega/filesystem.h
@@ -583,7 +583,7 @@ struct MEGA_API FileSystemAccess : public EventTrigger
     virtual bool issyncsupported(const LocalPath&, bool&, SyncError&, SyncWarning&) = 0;
 
     // get the absolute path corresponding to a path
-    virtual bool expanselocalpath(LocalPath& path, LocalPath& absolutepath) = 0;
+    virtual bool expanselocalpath(const LocalPath& path, LocalPath& absolutepath) = 0;
 
     // default permissions for new files
     int getdefaultfilepermissions() { return 0600; }
