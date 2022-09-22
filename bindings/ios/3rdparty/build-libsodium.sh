9d9ae0e1bc19a2def3340f5be27cdc7df8d9a12c
Javier Navarro <jn@mega.co.nz>
2022-09-22 13:30:34 +0200

    Update ios libsodium script to built the library for arm simulator and create xcframework

--- bindings/ios/3rdparty/build-libsodium.sh
+++ bindings/ios/3rdparty/build-libsodium.sh
@@ -5,8 +5,15 @@ SDKVERSION=`xcrun -sdk iphoneos --show-sdk-version`
 
 ##############################################
 CURRENTPATH=`pwd`
-ARCHS="i386 x86_64 armv7 armv7s arm64"
+ARCHS="x86_64 arm64 arm64-simulator"
 DEVELOPER=`xcode-select -print-path`
+
+CORES=$(sysctl -n hw.ncpu)
+
+# Formating
+green="\033[32m"
+bold="\033[0m${green}\033[1m"
+normal="\033[0m"
 
 if [ ! -d "$DEVELOPER" ]; then
   echo "xcode path is not set correctly $DEVELOPER does not exist (most likely because of xcode > 4.3)"
@@ -35,14 +42,18 @@ set -e
 
 if [ ! -e "libsodium-${LIBSODIUM_VERSION}.tar.gz" ]
 then
-wget "https://github.com/jedisct1/libsodium/releases/download/${LIBSODIUM_VERSION}/libsodium-${LIBSODIUM_VERSION}.tar.gz"
+curl -LO "https://github.com/jedisct1/libsodium/releases/download/${LIBSODIUM_VERSION}/libsodium-${LIBSODIUM_VERSION}.tar.gz"
 fi
 
 for ARCH in ${ARCHS}
 do
-if [[ "${ARCH}" == "i386" || "${ARCH}" == "x86_64" ]];
+if [[ "${ARCH}" == "x86_64" || "${ARCH}" == "arm64-simulator" ]];
 then
 PLATFORM="iPhoneSimulator"
+if [ "${ARCH}" == "arm64-simulator" ];
+then
+ARCH="arm64"
+fi
 else
 PLATFORM="iPhoneOS"
 fi
@@ -51,26 +62,35 @@ rm -rf libsodium-${LIBSODIUM_VERSION}
 tar zxf libsodium-${LIBSODIUM_VERSION}.tar.gz
 pushd "libsodium-${LIBSODIUM_VERSION}"
 
+
+echo "${bold}Building sodium for $PLATFORM $ARCH ${normal}"
+
 export BUILD_TOOLS="${DEVELOPER}"
 export BUILD_DEVROOT="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
 export BUILD_SDKROOT="${BUILD_DEVROOT}/SDKs/${PLATFORM}${SDKVERSION}.sdk"
+
+RUNTARGET=""
+if [[ "${ARCH}" == "arm64"  && "$PLATFORM" == "iPhoneSimulator" ]];
+then
+RUNTARGET="-target ${ARCH}-apple-ios13.0-simulator"
+fi
 
 export CC="${BUILD_TOOLS}/usr/bin/gcc -arch ${ARCH}"
 mkdir -p "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk"
 
 # Build
-export LDFLAGS="-Os -arch ${ARCH} -Wl,-dead_strip -miphoneos-version-min=9.0 -L${BUILD_SDKROOT}/usr/lib"
-export CFLAGS="-Os -arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${BUILD_SDKROOT} -miphoneos-version-min=9.0 -DNDEBUG"
+export LDFLAGS="-Os -arch ${ARCH} -Wl,-dead_strip -miphoneos-version-min=13.0 -L${BUILD_SDKROOT}/usr/lib"
+export CFLAGS="-Os -arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${BUILD_SDKROOT} -miphoneos-version-min=13.0 -DNDEBUG ${RUNTARGET}"
 export CPPFLAGS="${CFLAGS} -I${BUILD_SDKROOT}/usr/include"
 export CXXFLAGS="${CPPFLAGS}"
 
 if [ "${ARCH}" == "arm64" ]; then
-./configure --host=aarch64-apple-darwin --disable-shared --enable-minimal
+./configure --host=arm-apple-darwin --disable-shared --enable-minimal
 else
 ./configure --host=${ARCH}-apple-darwin --disable-shared --enable-minimal
 fi
 
-make -j8
+make -j${CORES}
 
 cp -f src/libsodium/.libs/libsodium.a ${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/
 
@@ -80,12 +100,20 @@ done
 
 
 mkdir lib || true
-lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-i386.sdk/libsodium.a ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-x86_64.sdk/libsodium.a  ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7.sdk/libsodium.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-armv7s.sdk/libsodium.a ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-arm64.sdk/libsodium.a -output ${CURRENTPATH}/lib/libsodium.a
+
+lipo -create ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-x86_64.sdk/libsodium.a ${CURRENTPATH}/bin/iPhoneSimulator${SDKVERSION}-arm64.sdk/libsodium.a -output ${CURRENTPATH}/bin/libsodium.a
+
+echo "${bold}Creating xcframework ${normal}"
+
+xcodebuild -create-xcframework -library ${CURRENTPATH}/bin/libsodium.a -library ${CURRENTPATH}/bin/iPhoneOS${SDKVERSION}-arm64.sdk/libsodium.a -output ${CURRENTPATH}/xcframework/libsodium.xcframework
 
 cp -fR libsodium-${LIBSODIUM_VERSION}/src/libsodium/include/sodium* include
+ 
+echo "${bold}Cleaning up ${normal}"
 
 rm -rf bin
 rm -rf libsodium-${LIBSODIUM_VERSION}
+rm -rf libsodium-${LIBSODIUM_VERSION}.tar.gz
 
 echo "Done."
 
