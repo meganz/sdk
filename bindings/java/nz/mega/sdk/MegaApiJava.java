a92a0e577ebc218b879c5df7178d8881375eda40
Yenel <yr@mega.nz>
2022-10-05 17:17:08 +0200

    SDK-2364 Remove MegaApi::getRecentActions() Android bindings

--- bindings/java/nz/mega/sdk/MegaApiJava.java
+++ bindings/java/nz/mega/sdk/MegaApiJava.java
@@ -9670,35 +9670,6 @@ public class MegaApiJava {
     }
 
     /**
-     * Return a list of buckets, each bucket containing a list of recently added/modified nodes
-     * <p>
-     * Each bucket contains files that were added/modified in a set, by a single user.
-     *
-     * @param days     Age of actions since added/modified nodes will be considered (in days)
-     * @param maxnodes Maximum amount of nodes to be considered
-     * @return List of buckets containing nodes that were added/modified as a set
-     */
-    public ArrayList<MegaRecentActionBucket> getRecentActions(long days, long maxnodes) {
-        return recentActionsToArray(megaApi.getRecentActions(days, maxnodes));
-    }
-
-    /**
-     * Return a list of buckets, each bucket containing a list of recently added/modified nodes
-     * <p>
-     * Each bucket contains files that were added/modified in a set, by a single user.
-     * <p>
-     * This function uses the default parameters for the MEGA apps, which consider (currently)
-     * interactions during the last 30 days and max 10.000 nodes.
-     * <p>
-     * You take the ownership of the returned value.
-     *
-     * @return List of buckets containing nodes that were added/modified as a set
-     */
-    public ArrayList<MegaRecentActionBucket> getRecentActions() {
-        return recentActionsToArray(megaApi.getRecentActions());
-    }
-
-    /**
      * Get a list of buckets, each bucket containing a list of recently added/modified nodes
      *
      * Each bucket contains files that were added/modified in a set, by a single user.
