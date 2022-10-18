a281f0d650d0e464677ad261832724873cf85b18
Matt Weir <mattw@mega.co.nz>
2022-10-18 14:05:48 +1300

    Also update megacli for changed functions

--- examples/megacli.cpp
+++ examples/megacli.cpp
@@ -10094,7 +10094,7 @@ void exec_syncremove(autocomplete::ACState& s)
         };
     }
 
-    client->syncs.removeSync(v[0].mBackupId, completion);
+    client->deregisterThenRemoveSync(v[0].mBackupId, completion);
 }
 
 void exec_syncstatus(autocomplete::ACState& s)
