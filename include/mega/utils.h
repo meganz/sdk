938c177ea4d4ea8c06e27d02b37f2e2e212dcc38
Javier Gonzalez Andres <jga@mega.nz>
2023-01-04 16:03:05 +0100

    Add methods to un/serialize uint8

--- include/mega/utils.h
+++ include/mega/utils.h
@@ -581,6 +581,7 @@ struct CacheableWriter
     void serializei64(int64_t field);
     void serializeu64(uint64_t field);
     void serializeu32(uint32_t field);
+    void serializeu8(uint8_t field);
     void serializehandle(handle field);
     void serializenodehandle(handle field);
     void serializefsfp(fsfp_t field);
@@ -613,6 +614,7 @@ struct CacheableReader
     bool unserializei32(int32_t& s);
     bool unserializei64(int64_t& s);
     bool unserializeu32(uint32_t& s);
+    bool unserializeu8(uint8_t& field);
     bool unserializeu64(uint64_t& s);
     bool unserializebyte(byte& s);
     bool unserializedouble(double& s);
