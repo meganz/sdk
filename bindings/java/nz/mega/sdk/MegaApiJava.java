77b52dda25c13662e2b74af449bd80ff13e45088
Jonathan Andy <jta@mega.co.nz>
2023-01-09 11:44:04 +1300

    SDK-2531: Add getSetElementCount support on Android side

--- bindings/java/nz/mega/sdk/MegaApiJava.java
+++ bindings/java/nz/mega/sdk/MegaApiJava.java
@@ -11895,6 +11895,18 @@ public class MegaApiJava {
     }
 
     /**
+     * @brief Get Element count of the Set with the given id, for current user.
+     *
+     * @param sid the id of the Set to get Element count for
+     *
+     * @return Element count of requested Set, or 0 if not found
+     */
+    public long getSetElementCount(long sid) {
+        return megaApi.getSetElementCount(sid);
+    }
+
+
+    /**
      * Get all Elements in the Set with given id, for current user.
      * <p>
      * The response value is stored as a MegaSetElementList.
