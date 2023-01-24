aa2e8fa3505278f658652866944fd823193ba864
Matt Weir <mattw@mega.co.nz>
2023-01-24 17:11:44 +1300

    rename sendPutnodes to sendPutnodesOfUpload, and add sendPutnodesToCloneNode

--- include/mega/file.h
+++ include/mega/file.h
@@ -26,7 +26,8 @@
 
 namespace mega {
 
-// file to be transferred
+// File is the base class for an upload or download, as managed by the SDK core.
+// Each Transfer consists of a list of File that all have the same content and fingerprint
 struct MEGA_API File: public FileFingerprint
 {
     // set localfilename in attached transfer
@@ -51,10 +52,15 @@ struct MEGA_API File: public FileFingerprint
     // update localname
     virtual void updatelocalname() { }
 
-    void sendPutnodes(MegaClient* client, UploadHandle fileAttrMatchHandle, const UploadToken& ultoken,
+    void sendPutnodesOfUpload(MegaClient* client, UploadHandle fileAttrMatchHandle, const UploadToken& ultoken,
                       const FileNodeKey& filekey, putsource_t source, NodeHandle ovHandle,
                       std::function<void(const Error&, targettype_t, vector<NewNode>&, bool targetOverride, int tag)>&& completion,
                       LocalNode* l, const m_time_t* overrideMtime, bool canChangeVault);
+
+    void sendPutnodesToCloneNode(MegaClient* client, Node* nodeToClone,
+                      putsource_t source, NodeHandle ovHandle,
+                      std::function<void(const Error&, targettype_t, vector<NewNode>&, bool targetOverride, int tag)>&& completion,
+                      bool canChangeVault);
 
     // generic filename for this transfer
     void displayname(string*);
