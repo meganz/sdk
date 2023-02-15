0953df00e80da783b0a5c9d8a7948ada9e0a4f68
Gene Thomas <gth@mega.co.nz>
2023-02-15 17:16:17 +1300

    LOG_err_if(condition) to LOG_err if true otherwise LOG_debug. No way to do this without a macro calling the SimpleLogger internals.

--- include/mega/logging.h
+++ include/mega/logging.h
@@ -660,6 +660,11 @@ std::ostream& operator <<(std::ostream&, const std::error_code&);
 #define LOG_fatal \
     ::mega::SimpleLogger(::mega::logFatal, ::mega::log_file_leafname(__FILE__), __LINE__)
 
+// LOG_err if the condition is true otherwise LOG_debug
+#define LOG_err_if(condition) \
+    if (::mega::SimpleLogger::logCurrentLevel >= ::mega::logDebug) \
+        ::mega::SimpleLogger((condition) ? ::mega::logError : ::mega::logDebug, ::mega::log_file_leafname(__FILE__), __LINE__)
+
 #if (defined(ANDROID) || defined(__ANDROID__))
 inline void crashlytics_log(const char* msg)
 {
