cbeb5364ef67d38480b438a7e525ffc11996c891
Javier Serrano <js@mega.co.nz>
2023-03-14 02:32:50 +0100

    Do not enable temporal authrings if there are no contacts

--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -14245,7 +14245,6 @@ void MegaClient::loadAuthrings()
 void MegaClient::fetchContactsKeys()
 {
     assert(mAuthRings.size() == 2);
-    mAuthRingsTemp = mAuthRings;
 
     // Populate mPendingContactKeys first, because if it's done just before fetchContactKeys,
     // it could finish synchronously and deactivate mAuthRingsTemp too early
@@ -14262,6 +14261,14 @@ void MegaClient::fetchContactsKeys()
         }
     }
 
+    if (pendingEdKeys.empty())
+    {
+        LOG_debug << "No need to fetch contact keys (no contacts)";
+        mPendingContactKeys.clear();
+        return;
+    }
+
+    mAuthRingsTemp = mAuthRings;
     for (auto &it : users)
     {
         User *user = &it.second;
