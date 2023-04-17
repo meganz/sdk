0ebc185868a9f37179f9f454cd077ef1b1d47c25
Víctor Manuel García Amicis <vmga@mega.nz>
2023-04-17 09:41:54 +0100

    Tag copyCachedStatus as deprecated.
    This method is related to CacheableStatus (where new values
    STATUS_JOURNEY_ID and STATUS_JOURNEY_TRACKING have been added, so it
    could need to be updated as well), but it is deprecated. It was used
    three years ago to migrate data from MegaSync to SDK cache.

--- include/megaapi.h
+++ include/megaapi.h
@@ -15088,7 +15088,10 @@ class MegaApi
          * @param blockStatus block status (0 = blocked, != 0 otherwise). Pass 999 if not valid
          * @param businessStatus business status. Pass 999 if not valid
          * @param listener MegaRequestListener to track this request
+         *
+         * @deprecated This function is deprecated. It was used years ago to migrate data from MegaSync to SDK cache.
          */
+        MEGA_DEPRECATED
         void copyCachedStatus(int storageStatus, int blockStatus, int businessStatus, MegaRequestListener *listener = NULL);
 
         /**
