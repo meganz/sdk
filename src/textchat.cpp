ba2a85edfcc9ed455d99798371f2dcb5e1dce8d7
Javier Gonzalez Andres <jga@mega.nz>
2023-06-13 16:24:24 +0200

    Improve logs

--- src/textchat.cpp
+++ src/textchat.cpp
@@ -1089,7 +1089,10 @@ bool TextChat::setMode(bool publicchat)
         return false;
     }
 
-    if (!publicchat) { LOG_debug << "TextChat::setMode: EKR enabled (private chat)"; }
+    if (!publicchat)
+    {
+        LOG_debug << "TextChat::setMode: EKR enabled (private chat) for chat: " << Base64Str<MegaClient::CHATHANDLE>(id);
+    }
     this->publicchat = publicchat;
     changed.mode = true;
 
