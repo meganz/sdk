25770f849b086c4da93c213c44d5db3d4cbad734
Jeff Ruan <cjr@mega.co.nz>
2023-07-04 16:33:28 +1200

    MacDirNotify supports unnormalized path

--- src/osx/fs.cpp
+++ src/osx/fs.cpp
@@ -127,12 +127,14 @@ MacDirNotify::MacDirNotify(const LocalPath& ignoreName,
     if (!mEventStream)
         return;
 
-    // How long is the root path?
-    mRootPathLength = rootPath.localpath.size();
-
-    // Exclude any trailing separator.
-    if (rootPath.endsInSeparator())
-        --mRootPathLength;
+    // How long is the normalized root path((like std::canonical and Exclude any trailing separator)?
+    LocalPath expanseRootPath;
+    if (!FSACCESS_CLASS().expanselocalpath(rootPath, expanseRootPath))
+    {
+        LOG_err << "Fail to expanseRootPath:" << rootPath.localpath;
+        return;
+    }
+    mRootPathLength = expanseRootPath.endsInSeparator() ? expanseRootPath.localpath.size() - 1 : expanseRootPath.localpath.size();
 
     // Specify who should process our filesystem events.
     FSEventStreamSetDispatchQueue(mEventStream, mOwner.mDispatchQueue);
