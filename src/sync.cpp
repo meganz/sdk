e7593cbfc38516ae448396993c7623cacfec5fab
Matt Weir <mattw@mega.co.nz>
2023-09-07 13:11:04 +1200

    SDK-3181. Altering a .megaignore to swap ignoring of a symlink needed a rescan afterward

--- src/sync.cpp
+++ src/sync.cpp
@@ -7041,8 +7041,10 @@ bool Sync::recursiveSync(SyncRow& row, SyncPath& fullPath, bool belowRemovedClou
 
                     if (childRow.syncNode)
                     {
-                        auto p = childRow.syncNode->getLocalPath();
-                        assert(0 == compareUtf(p, true, fullPath.localPath, true, mCaseInsensitive));
+                        #ifdef DEBUG
+                            auto p = childRow.syncNode->getLocalPath();
+                            assert(0 == compareUtf(p, true, fullPath.localPath, true, mCaseInsensitive));
+                        #endif
                         childRow.syncNode->reassignUnstableFsidsOnceOnly(childRow.fsNode);
                     }
 
