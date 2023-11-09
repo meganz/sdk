6842de52b1d94a4904f62c912f56c2f287dabe3f
Javier Navarro <jn@mega.co.nz>
2023-11-09 17:05:24 +0100

    Remove unneeded HStack

--- examples/SwiftUI/ExampleApp/ExampleApp/Modules/Presentation/Nodes/NodeView.swift
+++ examples/SwiftUI/ExampleApp/ExampleApp/Modules/Presentation/Nodes/NodeView.swift
@@ -5,14 +5,20 @@ struct NodeView: View {
     var viewModel: NodeViewModel
     
     var body: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                Text(viewModel.name)
-                    .font(.headline)
-                Text(viewModel.creationString)
-                    .font(.subheadline)
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
+        VStack(alignment: .leading) {
+            Text(viewModel.name)
+                .font(.headline)
+            Text(viewModel.creationString)
+                .font(.subheadline)
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
+}
+
+#Preview {
+    NodeView(viewModel: NodeViewModel(node: NodeEntity(name: "example",
+                                                       handle: 1,
+                                                       size: 1024,
+                                                       creationTime: Date.now,
+                                                       modificationTime: .now)))
 }
