224321e640a2c5931959fd7a92f208595f1d7cdb
Alfonso Abella <aag@mega.co.nz>
2023-11-10 11:20:44 +0100

    Apply APs of sharee leaving the share only if statecurrent
    If the last sharee leaves an outshare the change may not be notified and
    the share will not be present in the `f` response.
    Stop storing the APs related to sharees leaving a share and start
    syncing the sharekeys in-use bit with the effectively active shares
    before reaching statecurrent.

--- include/mega/megaclient.h
+++ include/mega/megaclient.h
@@ -2113,9 +2113,6 @@ public:
 
     KeyManager mKeyManager;
 
-    // Shares left by sharee, to review in-use bit in ^!keys before statecurrent
-    std::set<handle> mSharesToReview;
-
     // account access: master key
     // folder link access: folder key
     SymmCipher key;
--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -5318,36 +5318,10 @@ bool MegaClient::procsc()
                         uint64_t numNodes = mNodeManager.getNodeCount();
                         fnstats.nodesCurrent = numNodes;
 
-                        if(mSharesToReview.size() && mKeyManager.generation())
+                        if (mKeyManager.generation())
                         {
-                            // Ensure in-use bit in ^!keys is up to date for the not shared nodes.
-                            for(handle nh : mSharesToReview)
-                            {
-                                Node *n = nodebyhandle(nh);
-                                // Skip the nodes which are shared and the ones which are already cleared in ^!keys.
-                                if((n && n->isShared()) || !mKeyManager.isShareKeyInUse(nh))
-                                {
-                                    mSharesToReview.erase(nh);
-                                }
-                            }
-
-                            if(mSharesToReview.size())
-                            {
-                                mKeyManager.commit(
-                                    [this]()
-                                    {
-                                        string handles;
-                                        for(const handle nh : mSharesToReview)
-                                        {
-                                            mKeyManager.setSharekeyInUse(nh, false);
-                                            handles += toNodeHandle(nh);
-                                        }
-                                        LOG_debug << "Clearing in-use bit of the share-keys. Applied to: " << handles;
-                                    },[this]()
-                                    {
-                                        mSharesToReview.clear();
-                                    });
-                            }
+                            // Clear in-use bit if needed for the shared nodes in ^!keys.
+                            mKeyManager.syncSharekeyInUseBit();
                         }
 
                         statecurrent = true;
@@ -6690,11 +6664,6 @@ bool MegaClient::sc_shares()
                                     }
                                 }
                             }
-                        }
-                        else
-                        {
-                            // Store in the list to check status in ^!keys just before reaching statecurrent.
-                            mSharesToReview.insert(h);
                         }
                     }
 
