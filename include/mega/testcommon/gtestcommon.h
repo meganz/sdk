735d1a8b25118a895956734af916feddf5b23228
Catalin <cra@mega.nz>
2023-11-14 11:08:58 +0200

    Add ProcessWithInterceptedOutput class

--- include/mega/testcommon/gtestcommon.h
+++ include/mega/testcommon/gtestcommon.h
@@ -1,8 +1,37 @@
 #include <chrono>
+#include <functional>
+#include <memory>
 #include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "mega/process.h"
 
 namespace mega
 {
+
+class ProcessWithInterceptedOutput
+{
+public:
+    virtual ~ProcessWithInterceptedOutput() = default;
+    bool run(const std::vector<std::string>& args, const std::unordered_map<std::string, std::string>& env);
+    bool finishedRunning(); // false when not started or still running
+    int getExitCode(); // 0 for success
+
+protected:
+    virtual void clearBeforeRun() {} // override for member cleanup
+    virtual void onOutLine(std::string&& line);
+    virtual void onErrLine(std::string&& line);
+    virtual void onExit() {}
+
+private:
+    void intercept(const unsigned char* data, size_t len, std::string& buffer, std::function<void(std::string&&)> onLine);
+
+    std::unique_ptr<Process> mProc;
+    std::string mOutBuffer;
+    std::string mErrBuffer;
+    bool mExitReported = false;
+};
 
 std::string getCurrentTimestamp(bool includeDate = false);
 
--- src/testcommon/gtestcommon.cpp
+++ src/testcommon/gtestcommon.cpp
@@ -5,6 +5,131 @@ using namespace std;
 namespace mega
 {
 
+/// class ProcessWithInterceptedOutput
+///
+/// run a process and intercept its stdout and stderr before simply outputting them to console line-by-line
+
+bool ProcessWithInterceptedOutput::run(const vector<string>& args, const unordered_map<string, string>& env)
+{
+    // only run if not already running or if it finished
+    assert(!mProc || mProc->getPid() == -1 || mProc->hasStatus());
+    if (mProc && mProc->getPid() != -1 && !mProc->hasStatus())
+    {
+        return false;
+    }
+
+    // clean-up for previous run
+    mProc = make_unique<Process>();
+    mOutBuffer.clear();
+    mErrBuffer.clear();
+    mExitReported = false;
+    clearBeforeRun(); // other clean-up for derived classes
+
+    Process::DataReaderFunc interceptOut = [this](const unsigned char* data, size_t len)
+    {
+        intercept(data, len, mOutBuffer,
+                  [this](string&& line) { onOutLine(std::move(line)); });
+    };
+
+    Process::DataReaderFunc interceptErr = [this](const unsigned char* data, size_t len)
+    {
+        intercept(data, len, mErrBuffer,
+                  [this](string&& line) { onErrLine(std::move(line)); });
+    };
+
+    return mProc->run(args, env, interceptOut, interceptErr);
+}
+
+bool ProcessWithInterceptedOutput::finishedRunning()
+{
+    if (!mProc || mProc->getPid() == -1)
+    {
+        return false;
+    }
+
+    // "flushing" child process is mandatory; otherwise it might never report having exited;
+    // should probably be part of Process::isAlive()
+    mProc->poll();
+    mProc->flush();
+
+    return !mProc->isAlive();
+}
+
+int ProcessWithInterceptedOutput::getExitCode()
+{
+    if (!mProc || mProc->getPid() == -1)
+    {
+        return -1;
+    }
+
+    if (!mProc->hasStatus())
+    {
+        mProc->poll();
+        mProc->flush();
+        mProc->wait(); // not relevant if it did not start or failed or succeeded
+    }
+
+    // dump any remaining output
+    if (!mOutBuffer.empty())
+    {
+        onOutLine(std::move(mOutBuffer));
+        mOutBuffer.clear();
+    }
+    if (!mErrBuffer.empty())
+    {
+        onErrLine(std::move(mErrBuffer));
+        mErrBuffer.clear();
+    }
+
+    // react to the way it exited
+    if (!mExitReported)
+    {
+        mExitReported = true;
+        onExit();
+    }
+    return mProc->hasExited() ? mProc->getExitCode() : mProc->getTerminatingSignal();
+}
+
+void ProcessWithInterceptedOutput::onOutLine(string&& line)
+{
+    std::cout << line << std::endl;
+}
+void ProcessWithInterceptedOutput::onErrLine(string&& line)
+{
+    std::cerr << line << std::endl;
+}
+
+void ProcessWithInterceptedOutput::intercept(const unsigned char* data, size_t len, std::string& buffer, std::function<void(std::string&&)> onLine)
+{
+    if (!data || !len)
+    {
+        return;
+    }
+
+    buffer.append(reinterpret_cast<const char*>(data), len);
+
+    for (string::size_type lineStart = 0, lineEnd = buffer.find('\n', lineStart); ;
+         lineStart = lineEnd + 1, lineEnd = buffer.find('\n', lineStart))
+    {
+        // process only lines ending in separator
+        if (lineEnd == string::npos)
+        {
+            // leave the rest of the line in the buffer, just in case it doesn't finish with '\n'
+            buffer.erase(0, lineStart);
+            break;
+        }
+
+        string line = buffer.substr(lineStart, lineEnd - lineStart);
+        if (!line.empty() && line.back() == '\r') // Windows: \r\n
+        {
+            line.pop_back();
+        }
+
+        onLine(std::move(line));
+    }
+}
+
+
 std::string getCurrentTimestamp(bool includeDate)
 {
     using std::chrono::system_clock;
