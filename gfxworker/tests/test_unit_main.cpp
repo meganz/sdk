cdb1d193feba6527341d4b00b181bb85640b93ea
Jeff Ruan <cjr@mega.co.nz>
2023-11-30 15:51:09 +1300

    remove log to console

--- gfxworker/tests/test_unit_main.cpp
+++ gfxworker/tests/test_unit_main.cpp
@@ -4,7 +4,6 @@
 int main (int argc, char *argv[])
 {
     ::mega::MegaApi::setLogLevel(mega::MegaApi::LOG_LEVEL_MAX);
-    ::mega::MegaApi::setLogToConsole(true);
 
     testing::InitGoogleTest(&argc, argv);
     int rc = RUN_ALL_TESTS();
