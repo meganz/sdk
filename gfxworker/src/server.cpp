c170c6f705bd5cfabba99a7715a383ab1d3f6557
Jeff Ruan <cjr@mega.co.nz>
2023-12-11 19:28:38 +1300

    remove copyright in file

--- gfxworker/src/server.cpp
+++ gfxworker/src/server.cpp
@@ -1,22 +1,4 @@
-﻿/**
- * (c) 2013 by Mega Limited, Auckland, New Zealand
- *
- * This file is part of the MEGA SDK - Client Access Engine.
- *
- * Applications using the MEGA API must present a valid application key
- * and comply with the the rules set forth in the Terms of Service.
- *
- * The MEGA SDK is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * @copyright Simplified (2-clause) BSD License.
- *
- * You should have received a copy of the license along with this
- * program.
- */
-
-#include "server.h"
+﻿#include "server.h"
 #include "threadpool.h"
 #include "logger.h"
 #include "mega/filesystem.h"
