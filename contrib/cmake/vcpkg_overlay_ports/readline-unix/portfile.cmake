7587776f87f525cbc71a1cf95287c2a66717c67b
Alfonso Abella <aag@mega.co.nz>
2024-01-23 13:01:19 +0100

    Patch readline-unix overlay port
    Added a Find module to avoid using pkg-config, since the one from
    ncurses causes problems seting -mmacosx-version-min in MacOS.

--- contrib/cmake/vcpkg_overlay_ports/readline-unix/portfile.cmake
+++ contrib/cmake/vcpkg_overlay_ports/readline-unix/portfile.cmake
@@ -31,4 +31,7 @@ file(REMOVE_RECURSE
 
 vcpkg_fixup_pkgconfig()
 
+configure_file("${CMAKE_CURRENT_LIST_DIR}/FindReadline-unix.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/FindReadline-unix.cmake" @ONLY)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
