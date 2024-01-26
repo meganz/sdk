272bfd019dbd088f71f1d416cea7fdbff69257cb
Alfonso Abella <aag@mega.co.nz>
2024-01-26 19:43:27 +1300

    Accepted documentation changes

--- tests/integration/sdk_test_utils.h
+++ tests/integration/sdk_test_utils.h
@@ -6,14 +6,15 @@ namespace sdk_test
 {
 
 /**
- * @brief Returns the path to the folder containing the test executable.
+ * @brief Returns the path to the folder containing resources for the tests.
  *
  * IMPORTANT: `setTestDataDir` must be called before.
  */
 fs::path getTestDataDir();
 
 /**
- * @brief Sets the path to the folder where the test binary is located. You must call this inside main.
+ * @brief Sets the path to the folder where the test resources are located. Usually called from the main function.
+ * It should be set before calling getTestDataDir or copyFileFromTestData
  *
  * Example:
  *    setTestDataDir(fs::absolute(fs::path(argv[0]).parent_path()));
@@ -22,8 +23,8 @@ fs::path getTestDataDir();
 void setTestDataDir(const fs::path& dataDir);
 
 /**
- * @brief Copy file from the directory where the tests binary is located to the given destination (current working
- * directory by default).
+ * @brief Copies file from the resources data directory to the given destination (current working directory by default).
+ * IMPORTANT: `setTestDataDir` must be called before.
  */
 void copyFileFromTestData(fs::path filename, fs::path destination = ".");
 
