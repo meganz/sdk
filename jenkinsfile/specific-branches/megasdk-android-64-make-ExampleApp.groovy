8189f8850068deb0305b3a6df6c9a7e4515da1d0
Xavier Rins Lozano <xrl@mega.co.nz>
2024-02-02 12:05:31 +0100

    built 3rdparty libs every build

--- jenkinsfile/specific-branches/megasdk-android-64-make-ExampleApp.groovy
+++ jenkinsfile/specific-branches/megasdk-android-64-make-ExampleApp.groovy
@@ -8,22 +8,15 @@ pipeline {
         ANDROID_NDK_HOME = '/home/jenkins/android-ndk'
     }
     stages {
-        stage('Download prebuilt third-party-sources for example'){
-            steps {
-                dir("examples/android/ExampleApp/app/src/main/jni"){
-                    sh "jf rt download third-party-sources-sdk/3rdparty-sdk-android-example-all-archs.tar.gz ."
-                    sh "tar -xf 3rdparty-sdk-android-example-all-archs.tar.gz --skip-old-files"
-                }
-            }
-        }
         stage('build'){
             steps{
                 sh "export PATH=\$PATH:\$ANDROID_HOME/cmdline-tools/tools/bin/"
                 dir ("examples/android/ExampleApp/app/src/main/jni/") {
-                    sh "sed -i 's#JOBS=.*#JOBS=1#' build.sh"
+                    sh "sed -i \"s#-j[0-9]##g\" build.sh"
                     sh "sed -i 's#LOG_FILE=/dev/null#LOG_FILE=/dev/stdout#g' build.sh"
                     sh "rm -rf ../java/nz/mega/sdk/"
                     // Build libs and SDK.
+                    sh "bash -x ./build.sh clean"
                     sh "bash -x ./build.sh all"
                 }
                 dir ( "examples/android/ExampleApp") {
