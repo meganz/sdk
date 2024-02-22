2ae6a6b1e02553a104f7cd3e79361c21cdb8d503
Roberto Gómez <rg@mega.nz>
2024-02-22 19:31:50 +0100

    SDK-3676: iOS Bindings - Manage Last Read Notification

--- bindings/ios/MEGASdk.mm
+++ bindings/ios/MEGASdk.mm
@@ -1200,6 +1200,20 @@ using namespace mega;
     }
 }
 
+#pragma mark - Notifications
+
+- (void)getLastReadNotificationWithDelegate:(id<MEGARequestDelegate>)delegate {
+    if (self.megaApi) {
+        self.megaApi->getLastReadNotification([self createDelegateMEGARequestListener:delegate singleListener:YES]);
+    }
+}
+
+- (void)setLastReadNotificationWithNotificationId:(NSInteger)notificationId delegate:(id<MEGARequestDelegate>)delegate {
+    if (self.megaApi) {
+        self.megaApi->setLastReadNotification((int)notificationId, [self createDelegateMEGARequestListener:delegate singleListener:YES]);
+    }
+}
+
 #pragma mark - Filesystem changes Requests
 
 - (void)createFolderWithName:(NSString *)name parent:(MEGANode *)parent delegate:(id<MEGARequestDelegate>)delegate {
