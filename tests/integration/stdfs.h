6ff2b09c2002ca35f88e5350b5cfde546fae2cd4
Adrian Lozano <alo@mega.co.nz>
2024-02-28 09:43:12 +0100

    Stop using old filesystem include for macos

--- tests/integration/stdfs.h
+++ tests/integration/stdfs.h
@@ -1,16 +1,8 @@
 #pragma once
 
-#if (__cplusplus >= 201703L) && (!defined(__GNUC__) || (__GNUC__ * 100 + __GNUC_MINOR__) >= 800)
+#if (__cplusplus >= 201703L)
     #include <filesystem>
     namespace fs = std::filesystem;
-    #define USE_FILESYSTEM
-#elif (__cplusplus >= 201700L)
-    #include <experimental/filesystem>
-    namespace fs = std::experimental::filesystem;
-    #define USE_FILESYSTEM
-#elif (__clang_major__ >= 11)
-    #include <filesystem>
-    namespace fs = std::__fs::filesystem;
     #define USE_FILESYSTEM
 #elif !defined(__MINGW32__) && !defined(__ANDROID__) && (!defined(__GNUC__) || (__GNUC__*100+__GNUC_MINOR__) >= 503)
     #define USE_FILESYSTEM
@@ -22,4 +14,3 @@
         namespace fs = std::experimental::filesystem;
     #endif
 #endif
-
