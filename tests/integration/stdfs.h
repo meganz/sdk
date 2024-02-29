77d5ec72d957bbe545666700a04dee9d23367098
Adrian Lozano <alo@mega.co.nz>
2024-02-29 15:00:12 +0100

    Test directly including filesystem

--- tests/integration/stdfs.h
+++ tests/integration/stdfs.h
@@ -1,16 +1,5 @@
 #pragma once
 
-#if (__cplusplus >= 201703L)
-    #include <filesystem>
-    namespace fs = std::filesystem;
-    #define USE_FILESYSTEM
-#elif !defined(__MINGW32__) && !defined(__ANDROID__) && (!defined(__GNUC__) || (__GNUC__*100+__GNUC_MINOR__) >= 503)
-    #define USE_FILESYSTEM
-    #ifdef WIN32
-        #include <filesystem>
-        namespace fs = std::experimental::filesystem;
-    #else
-        #include <experimental/filesystem>
-        namespace fs = std::experimental::filesystem;
-    #endif
-#endif
+#include <filesystem>
+namespace fs = std::filesystem;
+#define USE_FILESYSTEM
