54477459558f42ff43e5a08f09215cf0a8cdd38e
kikadf <kikadf.01@gmail.com>
2024-03-12 17:09:11 +0100

    Add support other BSDs

--- include/mega/posix/megafs.h
+++ include/mega/posix/megafs.h
@@ -28,7 +28,7 @@
 #define O_DIRECT 0
 #include <sys/param.h>
 #include <sys/mount.h>
-#elif defined(__FreeBSD__)
+#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
 #include <sys/mount.h>
 #else
 #include <sys/vfs.h>
