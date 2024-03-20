5e068412effbcaabc2922594f72d3c8445f8d7f7
Jeff Ruan <cjr@mega.co.nz>
2024-03-20 16:09:29 +1300

    Move mSocket and mName to private

--- include/mega/posix/gfx/worker/comms.h
+++ include/mega/posix/gfx/worker/comms.h
@@ -18,17 +18,16 @@ public:
     bool isValid() const { return mSocket >= 0; }
 
     int fd() const { return mSocket; }
-protected:
-
-    // file descriptor to the socket
-    int mSocket{-1};
-
-    std::string mName;
 
 private:
     bool doWrite(const void* data, size_t n, TimeoutMs timeout) override;
 
     bool doRead(void* data, size_t n, TimeoutMs timeout) override;
+
+    // file descriptor to the socket
+    int mSocket{-1};
+
+    std::string mName;
 };
 
 } // end of namespace
