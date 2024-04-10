20d943ce19f83071642c63d43bfb0f3f20e89990
Alberto <ar@mega.nz>
2024-04-10 11:59:59 +0200

    Improve documetnation

--- include/mega/db/sqlite.h
+++ include/mega/db/sqlite.h
@@ -171,7 +171,7 @@ public:
     // Gets the mimetype corresponding to the file extension
     static void userGetMimetype(sqlite3_context* context, int argc, sqlite3_value** argv);
 
-    // Check if string (pattern) is contained at data base column from type text
+    // Check if string (pattern - argv[0]) is contained at data base column from type text (argv[1])
     static void userIsContained(sqlite3_context* context, int argc, sqlite3_value** argv);
 
 private:
