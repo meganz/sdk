86b115bfe7bf788d9d60c429d0d218b649fb2851
Víctor Manuel García Amicis <vmga@mega.nz>
2024-04-11 02:35:26 +0100

    SDK-3811. Fix RaidReq progress being greater than filesize
    The real size of each raid part can be slightly smaller than the padded
    part size (which is the size of the larger part size rounded to a
    RAIDSECTOR - 16 bytes). When we count the progress, if the position for
    a part is above the real part size, the difference must be substracted
    to get the real data progress.
    
    If the part is the parity part, it must take into account the real part
    size of the unused part (which is the one being reconstructed with
    parity).
    
    This was already implemented. When the part pos was equal to the padded
    part size, the difference was being subtracted. However, it wasn't being
    taken into account the rare case where a part is still inflight, and the
    part pos is greater than the part size, but still smaller than the
    padded part size.
    
    This commit fixes it and we know subtract the surplus.

--- src/raidproxy.cpp
+++ src/raidproxy.cpp
@@ -650,15 +650,18 @@ m_off_t PartFetcher::progress() const
     progressCount = totalReadAhead +
                     reqsProgress +
                     rr->mPartpos[part]; // Consecutive part data for not completed raidlines (i.e., other parts still not finished)
-    if (progressCount &&
-        (mPos + reqsProgress) == rr->mPaddedpartsize)
+    if (progressCount)
     {
         // Parts are padded to paddedpartsize, so we can only count as much as sourcesize (which is the real data size for that part).
-        assert(part != rr->unusedPart()); // If there is progress, it shouldn't be the unused part
-        auto partIndex = part == 0 ? rr->unusedPart() : part; // Parity must get the sourcesize of the unused part (for example, if part 5 is the unused, it can be smaller than part 0, so we must adjust the progress)
-        m_off_t paddedOffsetToSubstract = rr->mPaddedpartsize - rr->mFetcher[partIndex].mSourcesize;
-        progressCount -= paddedOffsetToSubstract;
-        assert ((progressCount + (rr->mDataline * RAIDSECTOR)) == rr->mFetcher[partIndex].mSourcesize);
+        assert(part != rr->unusedPart()); // If there is progress, this part shouldn't be the unused one.
+        auto partIndex = part == 0 ? rr->unusedPart() : part; // Parity must get the sourcesize of the unused part (for example, if part 5 is the unused one, it can be smaller than part 0, so we must adjust the progress)
+        m_off_t paddedOffsetToSubtract = (mPos + reqsProgress) - rr->mFetcher[partIndex].mSourcesize;
+        if (paddedOffsetToSubtract > 0) // The current position is above the source size, subtract it.
+        {
+            assert((mPos + reqsProgress) <= rr->mPaddedpartsize);
+            progressCount -= paddedOffsetToSubtract;
+            assert ((progressCount + (rr->mDataline * RAIDSECTOR)) == rr->mFetcher[partIndex].mSourcesize);
+        }
     }
     assert(progressCount >= 0);
     return progressCount;
