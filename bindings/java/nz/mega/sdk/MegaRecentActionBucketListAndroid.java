afdc0d476e798d8f8591d284acfca10192ef09d8
HM Tamim <ht@mega.co.nz>
2024-05-28 17:01:19 +0600

    SDK-3986: Android Bindings - Add Copy MegaRecentActionBucketList Binding

---
+++ bindings/java/nz/mega/sdk/MegaRecentActionBucketListAndroid.java
@@ -0,0 +1,14 @@
+package nz.mega.sdk;
+
+public class MegaRecentActionBucketListAndroid {
+
+    /**
+     * Creates a copy of MegaRecentActionBucketList.
+     *
+     * @param bucketList The MegaRecentActionBucketList received.
+     * @return A copy of MegaRecentActionBucketList.
+     */
+    public static MegaRecentActionBucketList copy(MegaRecentActionBucketList bucketList) {
+        return bucketList.copy();
+    }
+}
