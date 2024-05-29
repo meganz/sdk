13c1a399eac9797de2d93edf605c5d17bc3f8c68
David Ward <dw@mega.co.nz>
2024-05-29 13:52:56 +1200

    Moved (make)?ScopedValue into utils.h so we can use it in the SDK.

--- include/mega/utils.h
+++ include/mega/utils.h
@@ -1238,6 +1238,35 @@ SplitResult split(const char* begin, const char* end, char delimiter);
 SplitResult split(const char* begin, const std::size_t size, char delimiter);
 SplitResult split(const std::string& value, char delimiter);
 
+template<typename T>
+class ScopedValue {
+public:
+    ScopedValue(T& what, T value)
+      : mLastValue(std::move(what))
+      , mWhat(what)
+    {
+        what = std::move(value);
+    }
+
+    ~ScopedValue()
+    {
+        mWhat = std::move(mLastValue);
+    }
+
+    MEGA_DISABLE_COPY(ScopedValue)
+    MEGA_DEFAULT_MOVE(ScopedValue)
+
+private:
+    T mLastValue;
+    T& mWhat;
+}; // ScopedValue<T>
+
+template<typename T>
+ScopedValue<T> makeScopedValue(T& what, T value)
+{
+    return ScopedValue<T>(what, std::move(value));
+}
+
 } // namespace mega
 
 #endif // MEGA_UTILS_H
