6b6be4ff1f6323120733d8831a74c71b0b6d7d60
Jose Manuel Otero Mato <jom@mega.co.nz>
2024-06-19 18:03:49 +0200

    Added "sen" attribute list to delete when IN_SHARES

--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -8706,7 +8706,9 @@ error MegaClient::setattr(std::shared_ptr<Node> n, attr_map&& updates, CommandSe
     // Check and delete invalid fav attributes
     if (n->firstancestor()->getShareType() == ShareType_t::IN_SHARES) // Avoid an inshare to be tagged as favourite by the sharee
     {
-        std::vector<nameid> nameIds = { AttrMap::string2nameid("fav"), AttrMap::string2nameid("lbl") };
+        std::vector<nameid> nameIds = {AttrMap::string2nameid("fav"),
+                                       AttrMap::string2nameid("lbl"),
+                                       AttrMap::string2nameid("sen")};
         for (nameid& nameId : nameIds)
         {
             updates.erase(nameId);
