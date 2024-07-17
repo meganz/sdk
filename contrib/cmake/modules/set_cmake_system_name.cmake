71bcf11967809a479f8253f6aaaaf42e7ad4b04f
Jeff Ruan <cjr@mega.co.nz>
2024-07-17 14:42:00 +1200

    Rename vcpkg_target.cmake to set_cmake_system_name.cmake, rename check_vcpkg_target_triplet to set_cmake_system_name

--- contrib/cmake/modules/set_cmake_system_name.cmake
+++ contrib/cmake/modules/set_cmake_system_name.cmake
@@ -1,10 +1,10 @@
-# It sets CMAKE_SYSTEM_NAME based on target triplet
-# By convention, it is based the second part of the triplet
-# name. For example, the triplet arm64-ios's system name
-# is ios.
+# It sets CMAKE_SYSTEM_NAME based on target triplet.
+#
+# By convention, it is based the second part of the triplet name. For example,
+# the triplet arm64-ios's system name is ios.
 #
 # This function only sets for iOS
-function(check_vcpkg_target_triplet)
+function(set_cmake_system_name)
     if(VCPKG_TARGET_TRIPLET)
         # To list
         string(REPLACE "-" ";" TARGET_LIST ${VCPKG_TARGET_TRIPLET})
@@ -12,9 +12,9 @@ function(check_vcpkg_target_triplet)
         list(LENGTH TARGET_LIST LEN)
 
         if (${LEN} GREATER_EQUAL 2)
-            list(GET TARGET_LIST 1 OS_PART)
+            list(GET TARGET_LIST 1 SYSTEM_PART)
 
-            if(OS_PART MATCHES "^[Ii][Oo][Ss]$")
+            if(SYSTEM_PART MATCHES "^[Ii][Oo][Ss]$")
                 set(CMAKE_SYSTEM_NAME iOS CACHE STRING "")
             endif()
         endif()
