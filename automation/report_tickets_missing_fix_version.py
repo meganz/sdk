b55983292e2c2cc48e4111b2f89950dc976fad42
Adrian Lozano <alo@mega.co.nz>
2024-08-20 16:06:39 +0200

    Apply pep8 formatting

--- automation/report_tickets_missing_fix_version.py
+++ automation/report_tickets_missing_fix_version.py
@@ -19,6 +19,7 @@ jira_client = JIRA(JIRA_URL, token_auth=JIRA_API_TOKEN)
 # Initialize Slack client
 slack_client = WebClient(token=SLACK_TOKEN)
 
+
 def fetch_jira_issues(project_key: str) -> ResultList[Issue]:
     jql_query = f'project = {project_key} AND status = Resolved AND resolution = Done AND fixVersion is EMPTY'
     issues = jira_client.search_issues(
@@ -26,34 +27,39 @@ def fetch_jira_issues(project_key: str) -> ResultList[Issue]:
         fields='key, assignee',
         maxResults=200,
     )
-    
+
     return issues
 
-def get_slack_user_id_by_email(email: str) -> str|None:
+
+def get_slack_user_id_by_email(email: str) -> str | None:
     try:
         response = slack_client.users_lookupByEmail(email=email)
         return response['user']['id']
     except SlackApiError as e:
-        print(f"Error fetching Slack user ID for {email}: {e.response['error']}")
+        print(
+            f"Error fetching Slack user ID for {email}: {e.response['error']}")
         return None
+
 
 def post_to_slack(issue_key: str, slack_user_id: str):
     if not slack_user_id:
         user_mention = "<!channel>"
     else:
         user_mention = f"<@{slack_user_id}>"
-    
+
     issue_url = f"{JIRA_URL}/browse/{issue_key}"
     message = f"<{issue_url}|{issue_key}> is marked as *Resolved* but is missing a *Fix Version*.\n" \
-              f"CC: {user_mention}"
+        f"CC: {user_mention}"
 
     slack_client.chat_postMessage(channel=SLACK_CHANNEL, text=message)
     print(message)
+
 
 for project in JIRA_PROJECTS:
     issues = fetch_jira_issues(project)
     for issue in issues:
         issue_key = issue.key
         assignee = issue.fields.assignee
-        slack_user_id = get_slack_user_id_by_email(assignee.emailAddress) if assignee else None
+        slack_user_id = get_slack_user_id_by_email(
+            assignee.emailAddress) if assignee else None
         post_to_slack(issue_key, slack_user_id)
