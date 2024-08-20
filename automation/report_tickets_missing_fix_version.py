10c7c47be1fecec4591b83470d861e0199d01147
Adrian Lozano <alo@mega.co.nz>
2024-08-20 17:58:07 +0200

    Format python file using Black formatter

--- automation/report_tickets_missing_fix_version.py
+++ automation/report_tickets_missing_fix_version.py
@@ -5,13 +5,13 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 # Jira configuration
-JIRA_URL = 'https://jira.developers.mega.co.nz'
+JIRA_URL = "https://jira.developers.mega.co.nz"
 JIRA_API_TOKEN = os.environ["JIRA_PERSONAL_ACCESS_TOKEN"]
-JIRA_PROJECTS = ['SDK', 'CHT']
+JIRA_PROJECTS = ["SDK", "CHT"]
 
 # Slack configuration
 SLACK_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-SLACK_CHANNEL = '#test-slack-automation'
+SLACK_CHANNEL = "#test-slack-automation"
 
 # Initialize Jira client
 jira_client = JIRA(JIRA_URL, token_auth=JIRA_API_TOKEN)
@@ -21,10 +21,10 @@ slack_client = WebClient(token=SLACK_TOKEN)
 
 
 def fetch_jira_issues(project_key: str) -> ResultList[Issue]:
-    jql_query = f'project = {project_key} AND status = Resolved AND resolution = Done AND fixVersion is EMPTY'
+    jql_query = f"project = {project_key} AND status = Resolved AND resolution = Done AND fixVersion is EMPTY"
     issues = jira_client.search_issues(
         jql_query,
-        fields='key, assignee',
+        fields="key, assignee",
         maxResults=200,
     )
 
@@ -34,10 +34,9 @@ def fetch_jira_issues(project_key: str) -> ResultList[Issue]:
 def get_slack_user_id_by_email(email: str) -> str | None:
     try:
         response = slack_client.users_lookupByEmail(email=email)
-        return response['user']['id']
+        return response["user"]["id"]
     except SlackApiError as e:
-        print(
-            f"Error fetching Slack user ID for {email}: {e.response['error']}")
+        print(f"Error fetching Slack user ID for {email}: {e.response['error']}")
         return None
 
 
@@ -48,8 +47,10 @@ def post_to_slack(issue_key: str, slack_user_id: str):
         user_mention = f"<@{slack_user_id}>"
 
     issue_url = f"{JIRA_URL}/browse/{issue_key}"
-    message = f"<{issue_url}|{issue_key}> is marked as *Resolved* but is missing a *Fix Version*.\n" \
+    message = (
+        f"<{issue_url}|{issue_key}> is marked as *Resolved* but is missing a *Fix Version*.\n"
         f"CC: {user_mention}"
+    )
 
     slack_client.chat_postMessage(channel=SLACK_CHANNEL, text=message)
     print(message)
@@ -60,6 +61,7 @@ for project in JIRA_PROJECTS:
     for issue in issues:
         issue_key = issue.key
         assignee = issue.fields.assignee
-        slack_user_id = get_slack_user_id_by_email(
-            assignee.emailAddress) if assignee else None
+        slack_user_id = (
+            get_slack_user_id_by_email(assignee.emailAddress) if assignee else None
+        )
         post_to_slack(issue_key, slack_user_id)
