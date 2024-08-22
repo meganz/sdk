a2d7cf67058085d86bff23783945b79d36b70824
Adrian Lozano <alo@mega.co.nz>
2024-08-22 17:36:41 +0200

    Solve issues in previous ticket MR:
    	- Remove Jira internal link
    	- Use environment variables for every parameter
    	- Refactor script for easier usage and changes

--- automation/report_jira_tickets_on_slack.py
+++ automation/report_jira_tickets_on_slack.py
@@ -4,92 +4,110 @@ from jira.client import ResultList
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-# Jira configuration
-JIRA_URL = "https://jira.developers.mega.co.nz"
-JIRA_API_TOKEN = os.environ["JIRA_PERSONAL_ACCESS_TOKEN"]
-JIRA_PROJECTS = ["SDK", "CHT"]
 
-# Slack configuration
-SLACK_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-SLACK_CHANNEL = "#test-slack-automation"
+class JiraTicketsReporter:
 
-# Initialize Jira client
-jira_client = JIRA(JIRA_URL, token_auth=JIRA_API_TOKEN)
+    def __init__(
+        self,
+        jira_url: str,
+        jira_token: str,
+        jira_project_key: str,
+        slack_token: str,
+        slack_channel: str,
+    ):
+        self._jira_url = jira_url
+        self._jira_client = JIRA(self._jira_url, token_auth=jira_token)
+        self._jira_project_key = jira_project_key
+        self._slack_client = WebClient(token=slack_token)
+        self._slack_channel = slack_channel
 
-# Initialize Slack client
-slack_client = WebClient(token=SLACK_TOKEN)
+    def get_jira_query_for_issues_missing_fix_version(self, project_key: str):
+        return f"project = {project_key} AND status = Resolved AND resolution = Done AND fixVersion is EMPTY"
 
+    def get_jira_query_for_issues_missing_release_number_affected(
+        self, project_key: str
+    ):
+        return f'project = {project_key} AND status = Resolved AND resolution = Done AND fixVersion is not EMPTY AND "Release number affected" is EMPTY'
 
-def get_jira_query_for_issues_missing_fix_version(project_key: str):
-    return f"project = {project_key} AND status = Resolved AND resolution = Done AND fixVersion is EMPTY"
+    def fetch_jira_issues(self, jql_query: str) -> ResultList[Issue]:
+        issues = self._jira_client.search_issues(
+            jql_query,
+            fields="key, assignee",
+            maxResults=200,
+        )
 
+        return issues
 
-def get_jira_query_for_issues_missing_release_number_affected(project_key: str):
-    return f'project = {project_key} AND status = Resolved AND resolution = Done AND fixVersion is not EMPTY AND "Release number affected" is EMPTY'
+    def get_slack_user_id_by_email(self, email: str) -> str | None:
+        try:
+            response = self._slack_client.users_lookupByEmail(email=email)
+            return response["user"]["id"]
+        except SlackApiError as e:
+            print(f"Error fetching Slack user ID for {email}: {e.response['error']}")
+            return None
 
+    def post_to_slack(self, issue_key: str, slack_user_id: str, missing_field: str):
+        if not slack_user_id:
+            user_mention = "<!channel>"
+        else:
+            user_mention = f"<@{slack_user_id}>"
 
-def fetch_jira_issues(jql_query: str) -> ResultList[Issue]:
-    issues = jira_client.search_issues(
-        jql_query,
-        fields="key, assignee",
-        maxResults=200,
-    )
+        issue_url = f"{self._jira_url}/browse/{issue_key}"
+        message = (
+            f"<{issue_url}|{issue_key}> is marked as *Resolved* but is missing *{missing_field}*.\n"
+            f"CC: {user_mention}"
+        )
 
-    return issues
+        self._slack_client.chat_postMessage(channel=self._slack_channel, text=message)
+        print(message)
 
-
-def get_slack_user_id_by_email(email: str) -> str | None:
-    try:
-        response = slack_client.users_lookupByEmail(email=email)
-        return response["user"]["id"]
-    except SlackApiError as e:
-        print(f"Error fetching Slack user ID for {email}: {e.response['error']}")
-        return None
-
-
-def post_to_slack(issue_key: str, slack_user_id: str, missing_field: str):
-    if not slack_user_id:
-        user_mention = "<!channel>"
-    else:
-        user_mention = f"<@{slack_user_id}>"
-
-    issue_url = f"{JIRA_URL}/browse/{issue_key}"
-    message = (
-        f"<{issue_url}|{issue_key}> is marked as *Resolved* but is missing *{missing_field}*.\n"
-        f"CC: {user_mention}"
-    )
-
-    slack_client.chat_postMessage(channel=SLACK_CHANNEL, text=message)
-    print(message)
-
-
-def report_tickets_missing_fix_version():
-    for project in JIRA_PROJECTS:
-        issues = fetch_jira_issues(
-            get_jira_query_for_issues_missing_fix_version(project)
+    def report_tickets_missing_fix_version(self):
+        issues = self.fetch_jira_issues(
+            self.get_jira_query_for_issues_missing_fix_version(self._jira_project_key)
         )
         for issue in issues:
             issue_key = issue.key
             assignee = issue.fields.assignee
             slack_user_id = (
-                get_slack_user_id_by_email(assignee.emailAddress) if assignee else None
+                self.get_slack_user_id_by_email(assignee.emailAddress)
+                if assignee
+                else None
             )
-            post_to_slack(issue_key, slack_user_id, "Fix Version")
+            self.post_to_slack(issue_key, slack_user_id, "Fix Version")
 
-
-def report_tickets_missing_release_number_affected():
-    for project in JIRA_PROJECTS:
-        issues = fetch_jira_issues(
-            get_jira_query_for_issues_missing_release_number_affected(project)
+    def report_tickets_missing_release_number_affected(self):
+        issues = self.fetch_jira_issues(
+            self.get_jira_query_for_issues_missing_release_number_affected(
+                self._jira_project_key
+            )
         )
         for issue in issues:
             issue_key = issue.key
             assignee = issue.fields.assignee
             slack_user_id = (
-                get_slack_user_id_by_email(assignee.emailAddress) if assignee else None
+                self.get_slack_user_id_by_email(assignee.emailAddress)
+                if assignee
+                else None
             )
-            post_to_slack(issue_key, slack_user_id, "Release number affected")
+            self.post_to_slack(issue_key, slack_user_id, "Release number affected")
 
 
-report_tickets_missing_fix_version()
-report_tickets_missing_release_number_affected()
+# Get attribute values from the environment
+JIRA_URL = os.environ["JIRA_URL"]
+JIRA_PERSONAL_ACCESS_TOKEN = os.environ["JIRA_PERSONAL_ACCESS_TOKEN"]
+JIRA_PROJECT_KEY = os.environ["JIRA_PROJECT_KEY"]
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_CHANNEL = os.environ["SLACK_CHANNEL"]
+
+# Instantiate JiraTicketsReporter
+jiraTicketsReporter = JiraTicketsReporter(
+    jira_url=JIRA_URL,
+    jira_token=JIRA_PERSONAL_ACCESS_TOKEN,
+    jira_project_key=JIRA_PROJECT_KEY,
+    slack_token=SLACK_BOT_TOKEN,
+    slack_channel=SLACK_CHANNEL,
+)
+
+# Report issues
+jiraTicketsReporter.report_tickets_missing_fix_version()
+jiraTicketsReporter.report_tickets_missing_release_number_affected()
