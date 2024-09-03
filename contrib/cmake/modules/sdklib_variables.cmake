1347e743e9dca10ffa67e2a1f6a4b6deac48f1f4
Alfonso Abella <aag@mega.co.nz>
2024-09-03 13:22:45 +0200

    Adjust macOS min version

--- contrib/cmake/modules/sdklib_variables.cmake
+++ contrib/cmake/modules/sdklib_variables.cmake
@@ -9,3 +9,28 @@ if (NOT WIN32)
 else()
     set(USE_CPPTHREAD 1)
 endif()
+
+# Adjust default CMAKE_OSX_DEPLOYMENT_TARGET variable depending on target architecture
+# It should be set before calling project().
+if(NOT CMAKE_OSX_DEPLOYMENT_TARGET)
+    # CMAKE_OSX_DEPLOYMENT_TARGET also affects IOS builds
+    if(CMAKE_HOST_APPLE AND NOT (CMAKE_SYSTEM_NAME STREQUAL "iOS"))
+
+        # Minimum deployment target may differ if we are building for intel or arm64 targets
+        # CMAKE_SYSTEM_PROCESSOR and CMAKE_HOST_SYSTEM_PROCESSOR are only available after project()
+        execute_process(
+            COMMAND uname -m
+            OUTPUT_VARIABLE HOST_ARCHITECTURE
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+        if(CMAKE_OSX_ARCHITECTURES MATCHES "arm64" OR (NOT CMAKE_OSX_ARCHITECTURES AND HOST_ARCHITECTURE STREQUAL "arm64"))
+            set(CMAKE_OSX_DEPLOYMENT_TARGET "11.1" CACHE STRING "Minimum OS X deployment version")
+        else()
+            set(CMAKE_OSX_DEPLOYMENT_TARGET "10.13" CACHE STRING "Minimum OS X deployment version")
+        endif()
+
+        unset(HOST_ARCHITECTURE)
+
+    endif()
+endif()
