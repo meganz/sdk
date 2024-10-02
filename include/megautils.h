3b47f60e34ddab951153cba0b172ea9e04b26005
Alberto <ar@mega.nz>
2024-10-02 16:11:23 +0200

    Add brief to file megautils.h
    Adjust includes

--- include/megautils.h
+++ include/megautils.h
@@ -1,13 +1,13 @@
 /**
  * @file megautils.h
- * @brief Mega utilities
+ * @brief Utilities related with public objects from intermediate layer
  *
  * (c) 2024 by Mega Limited, Wellsford, New Zealand
  *
  * This file is part of the MEGA SDK - Client Access Engine.
  *
  * Applications using the MEGA API must present a valid application key
- * and comply with the the rules set forth in the Terms of Service.
+ * and comply with the rules set forth in the Terms of Service.
  *
  * The MEGA SDK is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -22,8 +22,9 @@
 #ifndef MEGAUTILS_H
 #define MEGAUTILS_H
 
-#include <functional>
 #include <megaapi.h>
+#include <string>
+#include <vector>
 
 namespace mega
 {
--- src/megautils.cpp
+++ src/megautils.cpp
@@ -1,13 +1,12 @@
 /**
  * @file megautils.cpp
- * @brief Mega utilities
  *
  * (c) 2024 by Mega Limited, Wellsford, New Zealand
  *
  * This file is part of the MEGA SDK - Client Access Engine.
  *
  * Applications using the MEGA API must present a valid application key
- * and comply with the the rules set forth in the Terms of Service.
+ * and comply with the rules set forth in the Terms of Service.
  *
  * The MEGA SDK is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,9 +19,6 @@
  */
 
 #include <megautils.h>
-#ifdef _WIN32
-#include <windows.h>
-#endif
 
 namespace mega
 {
