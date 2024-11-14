0bc2deadf2d0c6dedbaeac9f63b050ca873239c0
Cătălin Răceanu <cra@mega.nz>
2024-11-14 18:02:05 +0200

    Update hashcash.gencash test for the last changes and data sets

--- tests/unit/hashcash_test.cpp
+++ tests/unit/hashcash_test.cpp
@@ -25,10 +25,18 @@ std::string gencash(const std::string& token, uint8_t easiness);
 
 TEST(hashcash, gencash)
 {
-    static const std::string token =
-        "wFqIT_wY3tYKcrm5zqwaUoWym3ZCz32cCsrJOgYBgihtpaWUhGyWJ--EY-zfwI-i";
-    static const uint8_t easiness = 180;
-    static const std::string prefix = ::mega::gencash(token, easiness);
+    std::vector<std::tuple<std::string, uint8_t, std::string>> hashcash{
+        //{"wFqIT_wY3tYKcrm5zqwaUoWym3ZCz32cCsrJOgYBgihtpaWUhGyWJ--EY-zfwI-i", 180, "owAAAA"},
+        //{"HuTZK3FtxCopSWiLkgNz2_f0rlL8SIEtogZEkeCG63Q_tAcuMo_uccuQD4VVZTPx", 180, "9QYAAA"},
+        //{"3NIjq_fgu6bTyepwHuKiaB8a1YRjISBhktWK1fjhRx86RhOqKZNAcOZht0wJvmhQ", 180, "AQAAAA"},
+        {"Fhhm_MO7cOf5UhE4jT5roHS4Tb61SU_QcFkcx6u1KZh5IqSA5NhCIZMViPuT7Exu", 180, "AAAARw"},
+        {"l1_AfTmRRI05pmtF2Uf-wch8a8atZnrOcptIn0D79WIEfJo_qaFBZDqVuL3OOGPY", 180, "AAAAsA"},
+        {"PVWJ-N_pGddp240_lya9XqrxodPPa4tmmxSeZost5MkWdj6vg42E2O7VpbY6ibNs", 180, "AAABag"},
+    };
 
-    ASSERT_EQ(prefix, "owAAAA");
+    for (const auto& t : hashcash)
+    {
+        ASSERT_EQ(::mega::gencash(std::get<0>(t), std::get<1>(t)), std::get<2>(t));
+        std::cout << "found " << std::get<2>(t) << std::endl;
+    }
 }
