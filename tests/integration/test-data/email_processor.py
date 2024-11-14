4101666e1ff96d6384f5968dbcee59df2d0b0fc5
Alfonso Abella <aag@mega.co.nz>
2024-11-14 20:24:20 +0100

    Small refactor

--- tests/integration/test-data/email_processor.py
+++ tests/integration/test-data/email_processor.py
@@ -1,5 +1,5 @@
 
-# Extact a link from an email.
+# Extract a link from an email.
 # Used by sdk/test/integration test SdkTest.SdkTestCreateAccount
 import time
 SCRIPT_EXECUTION_TIME = time.time()
@@ -45,43 +45,60 @@ class EmailProcessor:
     def get_message_from_email(self, to, ref_time, delta=360.):
         """Get message from email."""
         # sort all emails sent to the right address, newest first
-        result, data = self.mail.sort('REVERSE DATE', 'UTF-8', '(To "{}")'.format(to))
-        if len(data[0]) == 0:
+        result, emails = self.mail.sort('REVERSE DATE', 'UTF-8', '(To "{}")'.format(to))
+        if len(emails[0]) == 0:
             return None
 
         # get all potential emails
         messages = []
-        for n in data[0].split():
-            _, d = self.mail.fetch(n, "(RFC822)")
-
-            for part in d:
-                if isinstance(part, tuple):
-                    msg = email.message_from_bytes(part[1])
-                    subject, encoding = email.header.decode_header(msg['subject'])[0]
-                    if isinstance(subject, bytes):
-                        subject = subject.decode(encoding)
-                    if self.debug: print("subject: " + subject)
-                    text = None
-                    dt = 0
-                    for item in msg['DKIM-Signature'].split(';'):
-                        if 't=' in item:
-                            dt = item.strip()[2:]
-                            break
-                    else:
-                        assert dt, 'timestamp not found from email header'
-                    elapsed = ref_time - float(dt)
-                    if elapsed > delta:
-                        # Emails are sorted by time so no need to continue processing
-                        break
-                    for m in msg.walk():
-                        content_type = m.get_content_type()
-                        if content_type == 'text/plain':
-                            text = m.get_payload(decode=True).decode('raw-unicode-escape')
-                            break
-                    if text:
-                        messages.append({subject: [n, text]})
-
+        for emailID in emails[0].split():
+            message = self.process_email(ref_time, emailID, delta)
+            if message:
+                messages.append(message)
         return messages
+
+    def process_email(self, ref_time, emailID, delta=360.):
+        """Process each piece of an email to find the body and subject"""
+        # Get email data from the ID.
+        _, data = self.mail.fetch(emailID, "(RFC822)")
+
+        for part in data:
+            # Look for a tuple. It will contain the email body. We can ignore the rest.
+            # It is usually in the first "part", but not always.
+            if isinstance(part, tuple):
+                # The body is the second in the tuple
+                msg = email.message_from_bytes(part[1])
+
+                # Process the subject
+                subject, encoding = email.header.decode_header(msg['subject'])[0]
+                if isinstance(subject, bytes):
+                    subject = subject.decode(encoding)
+                if self.debug: print("Subject: " + subject)
+
+                # Discard if it is outdated
+                dt = 0
+                for item in msg['DKIM-Signature'].split(';'):
+                    if 't=' in item:
+                        dt = item.strip()[2:]
+                        break
+                else:
+                    assert dt, 'timestamp not found from email header'
+                elapsed = ref_time - float(dt)
+                if elapsed > delta:
+                    # Emails are sorted by time so no need to continue processing
+                    break
+
+                # Get the plain text from the body
+                text = None
+                for m in msg.walk():
+                    content_type = m.get_content_type()
+                    if content_type == 'text/plain':
+                        text = m.get_payload(decode=True).decode('raw-unicode-escape')
+                        break
+
+                if subject and text:
+                    return {subject: [emailID, text]}
+        return None
 
 if len(sys.argv) == 1 or "--help" in sys.argv[1:] or (os.name == "nt" and "/?" in sys.argv[1:]):
     # no args, --help or /? on windows
