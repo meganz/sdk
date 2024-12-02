8a709205c79e84d2b4b3f2993585d2f761d8bccc
Jeff Ruan <cjr@mega.co.nz>
2024-12-02 15:20:57 +1300

    Add ShareSorterTest

---
+++ tests/unit/impl/share_test.cpp
@@ -0,0 +1,62 @@
+#include "impl/share.h"
+#include "mega/types.h"
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+using namespace ::mega::impl;
+using ::mega::Share;
+
+class ShareSorterTest: public ::testing::Test
+{
+protected:
+    void SetUp() override;
+
+    std::vector<Share> mShares;
+    std::vector<ShareData> mShareDatas;
+};
+
+void ShareSorterTest::SetUp()
+{
+    Test::SetUp();
+    mShares = {
+        Share{nullptr, mega::RDONLY, 20},
+        Share{nullptr, mega::RDONLY, 10},
+        Share{nullptr, mega::RDONLY, 30},
+    };
+    mShareDatas = {
+        {1, &mShares[0], true},
+        {2, &mShares[1], true},
+        {3, &mShares[2], true},
+    };
+}
+
+TEST_F(ShareSorterTest, SortByShareCreationTimeAscendingly)
+{
+    ShareSorter{::mega::MegaApi::ORDER_SHARE_CREATION_ASC}.sort(mShareDatas);
+    ASSERT_EQ(mShareDatas[0].getNodeHandle(), 3);
+    ASSERT_EQ(mShareDatas[1].getNodeHandle(), 1);
+    ASSERT_EQ(mShareDatas[2].getNodeHandle(), 2);
+}
+
+TEST_F(ShareSorterTest, SortByShareCreationTimeDescendingly)
+{
+    ShareSorter{::mega::MegaApi::ORDER_SHARE_CREATION_DESC}.sort(mShareDatas);
+    ASSERT_EQ(mShareDatas[0].getNodeHandle(), 2);
+    ASSERT_EQ(mShareDatas[1].getNodeHandle(), 1);
+    ASSERT_EQ(mShareDatas[2].getNodeHandle(), 3);
+}
+
+TEST_F(ShareSorterTest, SortByOthersDoesNotChangeOrder)
+{
+    ShareSorter{::mega::MegaApi::ORDER_NONE}.sort(mShareDatas);
+    ASSERT_EQ(mShareDatas[0].getNodeHandle(), 1);
+    ASSERT_EQ(mShareDatas[1].getNodeHandle(), 2);
+    ASSERT_EQ(mShareDatas[2].getNodeHandle(), 3);
+
+    ShareSorter{::mega::MegaApi::ORDER_CREATION_DESC}.sort(mShareDatas);
+    ASSERT_EQ(mShareDatas[0].getNodeHandle(), 1);
+    ASSERT_EQ(mShareDatas[1].getNodeHandle(), 2);
+    ASSERT_EQ(mShareDatas[2].getNodeHandle(), 3);
+}
