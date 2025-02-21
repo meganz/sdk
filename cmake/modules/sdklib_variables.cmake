4798739eb26ab75478d2125dc075f6b67f3c01dd
Antonio Borondo <abo@mega.co.nz>
2025-02-21 13:30:53 +0100

    Set CMAKE_SYSTEM_VERSION

--- cmake/modules/sdklib_variables.cmake
+++ cmake/modules/sdklib_variables.cmake
@@ -23,4 +23,5 @@ set_osx_deployment_target(
 if (CMAKE_SYSTEM_NAME STREQUAL "Android")
     # Ensure that compatibility with Android devices that use a 16KiB page size is enabled
     set(ANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES ON)
+    set(CMAKE_SYSTEM_VERSION 26)
 endif()
