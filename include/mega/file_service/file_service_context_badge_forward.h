4609f251bb504747423e1235325d0419e41fc7c6
David Ward <dw@mega.co.nz>
2025-04-08 10:30:59 +1200

    Added the FileServiceContextBadge class.

---
+++ include/mega/file_service/file_service_context_badge_forward.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <mega/common/badge_forward.h>
+#include <mega/file_service/file_service_context_forward.h>
+
+namespace mega
+{
+namespace file_service
+{
+
+using FileServiceContextBadge = common::Badge<FileServiceContext>;
+
+} // file_service
+} // mega
