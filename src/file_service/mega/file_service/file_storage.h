1afd63f9aecaba6d65acf9a198ecdb424677d0df
David Ward <dw@mega.co.nz>
2025-04-10 09:27:31 +1200

    Review: Explain purpose of openFile(...).

--- src/file_service/mega/file_service/file_storage.h
+++ src/file_service/mega/file_service/file_storage.h
@@ -11,6 +11,14 @@ namespace file_service
 
 class FileStorage
 {
+    // This function exists solely to reduce duplication.
+    //
+    // It is called by both addFile(...) and getFile(...).
+    //
+    // mustCreate specifies whether we need to create a new file for id.
+    //
+    // It's an error if mustCreate is true and a file for id already exists.
+    // It's an error if mustCreate is false but no file for id exists.
     auto openFile(FileID id, bool mustCreate) -> FileAccessPtr;
 
     auto userFilePath(FileID id) const -> LocalPath;
