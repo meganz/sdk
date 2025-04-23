41383e64958aaa34218d257eebad61ebab19e98e
David Ward <dw@mega.co.nz>
2025-04-24 09:08:16 +1200

    Added the IsValidKeyFunction<KeyFunction, ValueType> metafunction.
    As always, this'll be useful for sanity checking.

--- src/file_service/mega/file_service/file_range_tree_traits.h
+++ src/file_service/mega/file_service/file_range_tree_traits.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <mega/file_service/file_range_forward.h>
 #include <mega/file_service/file_range_traits.h>
+#include <mega/file_service/file_range_tree_traits.h>
 
 namespace mega
 {
@@ -8,6 +10,12 @@ namespace file_service
 {
 namespace detail
 {
+
+template<typename KeyFunction, typename ValueType>
+using IsValidKeyFunction = std::is_invocable_r<const FileRange&, KeyFunction, const ValueType&>;
+
+template<typename KeyFunction, typename ValueType>
+constexpr auto IsValidKeyFunctionV = IsValidKeyFunction<KeyFunction, ValueType>::value;
 
 template<typename ValueType>
 struct IsValidValueType: IsFileRange<ValueType>
--- src/file_service/testing/unit/file_range_tree_traits.cpp
+++ src/file_service/testing/unit/file_range_tree_traits.cpp
@@ -1,4 +1,6 @@
+#include <mega/file_service/file_range.h>
 #include <mega/file_service/file_range_tree_traits.h>
+#include <mega/file_service/type_traits.h>
 
 namespace mega
 {
@@ -6,6 +8,22 @@ namespace file_service
 {
 namespace detail
 {
+
+struct KeyFunctionA
+{
+    void operator()(const FileRange&) const;
+}; // KeyFunctionC
+
+struct KeyFunctionB
+{
+    const FileRange& operator()(int) const;
+}; // KeyFunctionD
+
+static_assert(IsValidKeyFunctionV<Identity, const FileRange>);
+static_assert(IsValidKeyFunctionV<SelectFirst, std::pair<const FileRange, int>>);
+
+static_assert(!IsValidKeyFunctionV<KeyFunctionA, const FileRange>);
+static_assert(!IsValidKeyFunctionV<KeyFunctionB, const FileRange>);
 
 static_assert(IsValidValueTypeV<const FileRange>);
 static_assert(IsValidValueTypeV<FileRange>);
