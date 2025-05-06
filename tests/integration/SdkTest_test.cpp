11000fe14792826203fb305aa19c2707dd01a753
Alberto <ar@mega.nz>
2025-05-06 12:07:02 +0200

    Fix compilation error in Macos at automatic tests

--- tests/integration/SdkTest_test.cpp
+++ tests/integration/SdkTest_test.cpp
@@ -20404,7 +20404,7 @@ TEST_F(SdkTest, SdkTestRemovePublicLinkSet)
     ASSERT_NO_FATAL_FAILURE(fetchnodes(secondaryClientIdx));
 
     const MrProper cleanUp(
-        [this, secondaryClientIdx]()
+        [this]()
         {
             // release secondary instance to avoid failure at tear down
             releaseMegaApi(secondaryClientIdx);
@@ -20424,7 +20424,7 @@ TEST_F(SdkTest, SdkTestRemovePublicLinkSet)
 
     LOG_debug << "Set handle: " << Base64Str<MegaClient::USERHANDLE>(sh);
 
-    auto exportSet = [this, sh, primaryClientIdx, secondaryClientIdx]()
+    auto exportSet = [this, sh]()
     {
         std::unique_ptr<MegaSet> set{megaApi[primaryClientIdx]->getSet(sh)};
         mApi[secondaryClientIdx].setUpdated = false;
@@ -20441,7 +20441,7 @@ TEST_F(SdkTest, SdkTestRemovePublicLinkSet)
         ASSERT_TRUE(setSecondAccount->getLinkDeletionReason() == MegaSet::DELETION_LINK_NO_REMOVED);
     };
 
-    auto disableExportSet = [this, sh, primaryClientIdx, secondaryClientIdx]()
+    auto disableExportSet = [this, sh]()
     {
         std::unique_ptr<MegaSet> set{megaApi[primaryClientIdx]->getSet(sh)};
         mApi[secondaryClientIdx].setUpdated = false;
@@ -20457,7 +20457,7 @@ TEST_F(SdkTest, SdkTestRemovePublicLinkSet)
         ASSERT_FALSE(setSecondAccount->isExported());
     };
 
-    auto checkDeletionReasonAfterResumeSession = [this, sh, primaryClientIdx](bool exported)
+    auto checkDeletionReasonAfterResumeSession = [this, sh](bool exported)
     {
         PerApi& target = mApi[primaryClientIdx];
         target.resetlastEvent();
