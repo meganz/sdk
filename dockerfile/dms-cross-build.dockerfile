0fc7f230555003dffe2612581fae5ea4e80ad8bc
Xavier Rins <xrl@mega.co.nz>
2025-05-08 10:59:15 +0200

    move vcpkg cache to s4 bucket

--- dockerfile/dms-cross-build.dockerfile
+++ dockerfile/dms-cross-build.dockerfile
@@ -41,6 +41,12 @@ RUN apt-get --quiet=2 update && DEBCONF_NOWARNINGS=yes apt-get --quiet=2 install
     zip \
     1> /dev/null
 
+# Install AWS CLI v2
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
+unzip awscliv2.zip && \
+./aws/install && \
+rm -rf awscliv2.zip aws
+
 # Set up work directory
 WORKDIR /mega
 
--- jenkinsfile/specific-branches/megasdk-debian64-gtests.groovy
+++ jenkinsfile/specific-branches/megasdk-debian64-gtests.groovy
@@ -11,6 +11,10 @@ pipeline {
             environment{
                 VCPKGPATH = "/opt/vcpkg"
                 BUILD_DIR = "build_dir"
+                VCPKG_BINARY_SOURCES  = 'clear;x-aws,s3://vcpkg-cache/archives/,readwrite'
+                AWS_ACCESS_KEY_ID     = credentials('s4_access_key_id_vcpkg_cache')
+                AWS_SECRET_ACCESS_KEY = credentials('s4_secret_access_key_vcpkg_cache')
+                AWS_ENDPOINT_URL      = "https://s3.g.s4.mega.io"
             }
             steps{
                 sh "rm -rf ${BUILD_DIR}; mkdir ${BUILD_DIR}"
