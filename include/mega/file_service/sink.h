20b561233b9cbe4579a19bfb2adbacfb4e485d72
David Ward <dw@mega.co.nz>
2025-05-26 08:05:45 +1200

    Added the Sink class.

---
+++ include/mega/file_service/sink.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <mega/file_service/sink_forward.h>
+
+#include <cstdint>
+
+namespace mega
+{
+namespace file_service
+{
+
+class Sink
+{
+protected:
+    Sink() = default;
+
+public:
+    virtual ~Sink() = default;
+
+    // Write data into the sink.
+    virtual bool write(const void* buffer, std::uint64_t offset, std::uint64_t length) = 0;
+}; // Sink
+
+} // file_service
+} // mega
