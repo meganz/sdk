dd52546eb5a342d291d9be1376841922c006dc63
Antonio Borondo <abo@mega.co.nz>
2025-05-30 16:03:11 +0200

    Set Windows 8 as the minimum Windows version

--- cmake/modules/configuration.cmake
+++ cmake/modules/configuration.cmake
@@ -18,11 +18,11 @@ if (MSVC)
     # Create a separated PDB file with debug symbols.
     add_compile_options($<$<CONFIG:Release>:/Zi>)
 
-    # Define _WIN32_WINNT to specify the minimum supported Windows version. 0x0601
-    # corresponds to Windows 7. Refer to sdkddkver.h in the Windows SDK for other
-    # possible values. WINVER and NTDDI_VERSION are set automatically in sdkddkver.h.
+    # Define _WIN32_WINNT to specify the minimum supported Windows version. 0x0602 corresponds to
+    # Windows 8. Refer to sdkddkver.h in the Windows SDK for other possible values. WINVER and
+    # NTDDI_VERSION are set automatically in sdkddkver.h.
     add_compile_definitions(
-        _WIN32_WINNT=0x0601
+        _WIN32_WINNT=0x0602
     )
 else()
     include(CheckIncludeFile)
