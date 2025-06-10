fe022eb7cdbb67c3058ca58d30b778471b293224
David Ward <dw@mega.co.nz>
2025-06-10 14:20:36 +1200

    Added the FileAppendRequest class.

---
+++ src/file_service/mega/file_service/file_append_request.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <mega/file_service/file_append_request_forward.h>
+#include <mega/file_service/file_callbacks.h>
+
+#include <cstdint>
+
+namespace mega
+{
+namespace file_service
+{
+
+struct FileAppendRequest
+{
+    // What data does the user want to append to the file?
+    const void* mBuffer;
+
+    // Who should we call when the append completes?
+    FileAppendCallback mCallback;
+
+    // How much data does the user want to append?
+    std::uint64_t mLength;
+}; // FileAppendRequest
+
+} // file_service
+} // mega
