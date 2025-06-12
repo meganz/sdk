52a98834bfd78559e2ff6ebe84a941d9b44d0497
David Ward <dw@mega.co.nz>
2025-06-13 11:01:54 +1200

    Added the FileFlushRequest class.

---
+++ src/file_service/mega/file_service/file_flush_request.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <mega/file_service/file_callbacks.h>
+#include <mega/file_service/file_flush_request_forward.h>
+#include <mega/file_service/file_request_tags.h>
+
+namespace mega
+{
+namespace file_service
+{
+
+struct FileFlushRequest
+{
+    // What kind of request is this?
+    using Type = FileReadRequestTag;
+
+    // This request's human readable name.
+    static const char* name()
+    {
+        return "flush";
+    }
+
+    // Who should we call when the flush completes?
+    FileFlushCallback mCallback;
+}; // FileFlushRequest
+
+} // file_service
+} // mega
