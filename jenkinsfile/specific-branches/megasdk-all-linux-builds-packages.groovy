b67f4bdf537dcd05abf94db4131608e393faffc0
Roddy González <rbg@mega.co.nz>
2025-06-20 12:30:11 +0200

    Disable nightly builds for Fedora_42 arm64

--- jenkinsfile/specific-branches/megasdk-all-linux-builds-packages.groovy
+++ jenkinsfile/specific-branches/megasdk-all-linux-builds-packages.groovy
@@ -134,7 +134,7 @@ pipeline {
                         } 
                         axis { 
                             name 'DISTRO'; 
-                            values  'DEB_Arch_Extra','Debian_11','xUbuntu_20.04','xUbuntu_24.04','Fedora_40',
+                            values  'DEB_Arch_Extra','Debian_11','xUbuntu_20.04','xUbuntu_24.04','Fedora_40', 'Fedora_42',
                                     'Raspbian_11', 'Raspbian_12','openSUSE_Tumbleweed'
                         }
                     }
