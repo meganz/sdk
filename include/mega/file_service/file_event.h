4bae2727bd316aaf52a75efdbe3f32f963b8176b
David Ward <dw@mega.co.nz>
2025-07-08 10:52:04 +1200

    Added the FileEvent class.

---
+++ include/mega/file_service/file_event.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <mega/file_service/file_event_forward.h>
+#include <mega/file_service/file_range.h>
+
+#include <cstdint>
+#include <optional>
+
+namespace mega
+{
+namespace file_service
+{
+
+struct FileEvent
+{
+    // What range, if any, has been modified?
+    std::optional<FileRange> mRange;
+
+    // What is the file's current modification time?
+    std::uint64_t mModified;
+
+    // What is the file's current size?
+    std::uint64_t mSize;
+}; // FileEvent
+
+} // file_service
+} // mega
---
+++ include/mega/file_service/file_event_forward.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace mega
+{
+namespace file_service
+{
+
+struct FileEvent;
+
+} // file_service
+} // mega
