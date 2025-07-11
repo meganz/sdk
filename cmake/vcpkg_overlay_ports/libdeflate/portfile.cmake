6f39bdb8454c79cf74de1bf2f0fe6d9c5ff5319f
Antonio Borondo <abo@mega.co.nz>
2025-07-11 13:19:41 +0200

    Patch libdeflate

--- cmake/vcpkg_overlay_ports/libdeflate/portfile.cmake
+++ cmake/vcpkg_overlay_ports/libdeflate/portfile.cmake
@@ -6,6 +6,7 @@ vcpkg_from_github(
     HEAD_REF master
     PATCHES
         remove_wrong_c_flags_modification.diff
+        msvc_dotprod.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
