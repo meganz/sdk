914eca0d5202d8eca9b0e3172f5089e63d7a0c5a
Alfonso Abella <aag@mega.co.nz>
2025-07-18 09:06:00 +0200

    Provide .pc file for system zlib

--- cmake/vcpkg_overlay_ports/ios_overlays/zlib/portfile.cmake
+++ cmake/vcpkg_overlay_ports/ios_overlays/zlib/portfile.cmake
@@ -1,2 +1,12 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-message(NOTICE "Skip ${PORT} build. VCPKG will try to use the ${PORT} provided by the system.")
+
+if(VCPKG_OSX_SYSROOT STREQUAL "iphonesimulator")
+    set(target_environment "iPhoneSimulator")
+else()
+    set(target_environment "iPhoneOS")
+endif()
+
+message(NOTICE "Creating .pc file for ${PORT}, pointing to the zlib provided by the system in the ${target_environment} sysroot/SDK")
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/zlib.pc.in" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/zlib.pc" @ONLY)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/zlib.pc.in" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/zlib.pc" @ONLY)
