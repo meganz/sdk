146708c964ce6a52a8808c75228f8f9cb5ec2778
Javier Navarro <jn@mega.co.nz>
2025-07-21 13:13:09 +0200

    Fix compilation for macOS SwiftUI demo

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "MEGASDK",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
