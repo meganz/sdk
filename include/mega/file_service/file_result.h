40fe922576f482e445e569d2b3508c87fc58cd60
David Ward <dw@mega.co.nz>
2025-09-30 08:53:38 +1300

    Added the FILE_READONLY result.

--- include/mega/file_service/file_result.h
+++ include/mega/file_service/file_result.h
@@ -14,6 +14,7 @@ namespace file_service
     expander(INVALID_ARGUMENTS, "The file operation was passed invalid arguments") \
     expander(CANCELLED, "The file operation has been cancelled") \
     expander(FAILED, "The file operation has failed") \
+    expander(READONLY, "The operation has failed because the file is read only") \
     expander(REMOVED, "The operation failed because the file has been removed") \
     expander(SUCCESS, "The file operation has succeeded")
 
