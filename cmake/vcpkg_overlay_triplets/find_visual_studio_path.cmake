471c85a83a08a355b68bdfb1e40871a80d46440c
l00140627 <liangchongyi@outlook.com>
2025-10-07 20:32:39 +0800

    support visual studio 2026 insider compiling

--- cmake/vcpkg_overlay_triplets/find_visual_studio_path.cmake
+++ cmake/vcpkg_overlay_triplets/find_visual_studio_path.cmake
@@ -1,5 +1,9 @@
 function(find_visual_studio_path)
     set(VISUAL_STUDIO_PATHS
+        "C:\\Program Files\\Microsoft Visual Studio\\2026\\Enterprise"
+        "C:\\Program Files\\Microsoft Visual Studio\\2026\\Professional" 
+        "C:\\Program Files\\Microsoft Visual Studio\\2026\\Community"
+        "C:\\Program Files\\Microsoft Visual Studio\\18\\Insiders"
         "C:\\Program Files\\Microsoft Visual Studio\\2022\\Enterprise"
         "C:\\Program Files\\Microsoft Visual Studio\\2022\\Professional"
         "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community"
@@ -11,11 +15,12 @@ function(find_visual_studio_path)
         if(EXISTS ${VISUAL_STUDIO_PATH})
             set(VISUAL_STUDIO_PATH ${VISUAL_STUDIO_PATH} PARENT_SCOPE)
             set(VISUAL_STUDIO_FOUND TRUE)
+            message(STATUS "Found Visual Studio at: ${VISUAL_STUDIO_PATH}")
             break()
         endif()
     endforeach()
 
     if(NOT VISUAL_STUDIO_FOUND)
-        message(FATAL_ERROR "No compatible Visual Studio 2022 installation found.")
+        message(FATAL_ERROR "No compatible Visual Studio 2026 or 2022 installation found.")
     endif()
 endfunction()
