f44f8540ab7df93d7e0af95e18585489cbc15a45
David Ward <dw@mega.co.nz>
2025-10-30 13:58:17 +1300

    Added missing Database documentation.

--- include/mega/common/database.h
+++ include/mega/common/database.h
@@ -29,10 +29,16 @@ struct LockableTraits<Database>
 class Database
   : public Lockable<Database>
 {
+    // See execute below.
     std::string execute(const char* statement);
 
+    // The database's SQL context.
     sqlite3* mDB;
+
+    // The databse's logger instance.
     Logger* mLogger;
+
+    // The database file this instance is accessing.
     std::string mPath;
 
 public:
@@ -42,15 +48,26 @@ public:
 
     ~Database();
 
+    // Directly execute an SQL statement on this database.
+    //
+    // If the statement caused an error, a message describing that error is
+    // returned to the caller.
+    //
+    // If the statement executed successfully, an empty string is returned
+    // to the caller.
     std::string execute(Badge<Transaction> badge, const char* statement);
 
+    // Retrieve a reference to this database's SQL context.
     sqlite3* get(Badge<Query> badge);
     sqlite3* get(Badge<Transaction> badge);
 
+    // Retrieve a reference to this database's logger.
     Logger& logger() const;
 
+    // Return a new query that references this database.
     Query query();
 
+    // Return a new transaction that references this database.
     Transaction transaction();
 }; // Database
 
