308a6de4816a3fa3703bd086e676147f1b1327b5
Robin Shi <rsh@mega.co.nz>
2025-12-25 12:06:17 +1300

    Upgrade Java API from 17 to 21
    - Update sourceCompatibility and targetCompatibility to JavaVersion.VERSION_21
    - Update Kotlin jvmTarget to 21
    - Update Kotlin version from 1.8.20 to 1.9.22 to support JVM target 21

--- examples/android/ExampleApp/build.gradle.kts
+++ examples/android/ExampleApp/build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.9.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
