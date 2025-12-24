8b20c660f5a8b8e1d81edb7e3962b5e925ed2272
Robin Shi <rsh@mega.co.nz>
2025-12-25 11:57:44 +1300

    Convert build.gradle to Kotlin DSL (build.gradle.kts)

--- examples/android/ExampleApp/build.gradle.kts
+++ examples/android/ExampleApp/build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         google()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.9.0'
-        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20'
+        classpath("com.android.tools.build:gradle:8.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -20,7 +20,7 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
 
