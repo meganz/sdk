3d77bf5476f6e2800580f62cd8a518b2f6a07749
Robin Shi <rsh@mega.co.nz>
2025-12-25 13:37:28 +1300

    Upgrade Android Gradle Plugin and Kotlin to latest versions
    - Upgrade Android Gradle Plugin from 8.9.0 to 8.12.0 (latest stable)
    - Upgrade Kotlin Gradle Plugin from 1.9.22 to 2.0.21 (latest stable)
    - Build validated and app tested successfully with updated versions

--- examples/android/ExampleApp/build.gradle.kts
+++ examples/android/ExampleApp/build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.9.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.android.tools.build:gradle:8.12.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
